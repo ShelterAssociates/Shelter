500b087c9955f8f617b0e91dccf8434eef23f451
PratikDhanave <i.pratikdhanave@gmail.com>
2016-05-25 18:19:20 +0530

    google map widget

--- master/forms.py
+++ master/forms.py
@@ -8,7 +8,11 @@ import json
 from django import forms
 from django.conf import settings
 
-from master.models import Survey
+from master.models import Survey,City
+from django.utils.safestring import mark_safe
+from django.template.loader import render_to_string
+from django.forms import widgets
+
 
 SURVEY_LIST = []
 
@@ -86,3 +90,32 @@ def get_kobo_id_list():
         temp_arr.append((value['id_string'], value['id_string']))
 
     return temp_arr
+
+
+class LocationWidget(widgets.TextInput):
+    """Map Widget"""
+    template_name = 'location.html'
+    def render(self, name, value, attrs=None):
+        context = {
+        'POLYGON' : value
+        }
+        return mark_safe(render_to_string(self.template_name, context))
+
+
+class CityFrom(forms.ModelForm):
+    """City Form"""
+    shape = forms.CharField(widget = LocationWidget())
+    class Meta:
+        model = City
+        fields = ('name', 'shape', 'state_code', 'district_code', 'city_code')
+        exclude = ('created_by', 'created_on')
+
+
+
+
+
+
+
+
+
+
