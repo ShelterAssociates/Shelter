567c0806f71860a32c8c56828f85c367e7eed1dc
PratikDhanave <i.pratikdhanave@gmail.com>
2016-07-06 15:39:29 +0530

    signal

--- master/signals.py
+++ master/signals.py
@@ -5,13 +5,12 @@
 import json
 import psycopg2
 
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.conf import settings
 
 from master.models import Survey, Slum, Rapid_Slum_Appraisal
 from bs4 import BeautifulSoup as Soup
-
 
 
 @receiver(post_save, sender=Slum)
@@ -66,9 +65,65 @@ def slum_created_trigger(sender, instance, **kwargs):
 
 
 
-@receiver(pre_save,sender=Rapid_Slum_Appraisal)
-def my_handler(sender,instance,self, **kwargs):
-    print sender
-    print kwargs
-    print instance
-    print self
+@receiver(post_save,sender=Rapid_Slum_Appraisal)
+def Rapid_Slum_Appraisa_created_trigger(sender,instance, **kwargs):
+    print "I am in Rapid_Slum_Appraisal"
+    this = Rapid_Slum_Appraisal.objects.get(id=instance.id)
+    try:
+        if this.gutter_info_left_image.name != instance.gutter_info_left_image.name:
+            print "I am in try"
+            this.gutter_info_left_image.delete(save=False)                
+    except:
+        print "I am in except"
+        pass # when new photo then we do nothing, normal case          
+ 
+
+
+
+
+
+"""
+    try:
+        if this.water_info_left_image.name != instance.water_info_left_image.name:
+            print "I am in try"
+            this.water_info_left_image.delete(save=False)                
+    except:
+        print "I am in except"
+        pass # when new photo then we do nothing, normal case          
+    try:
+        if this.waste_management_info_left_image.name != instance.waste_management_info_left_image.name:
+            print "I am in try"
+            this.waste_management_info_left_image.delete(save=False)                
+    except:
+        print "I am in except"
+        pass # when new photo then we do nothing, normal case          
+    try:
+        if this.toilet_info_left_image.name != instance.toilet_info_left_image.name:
+            print "I am in try"
+            this.toilet_info_left_image.delete(save=False)                
+    except:
+        print "I am in except"
+        pass # when new photo then we do nothing, normal case          
+    try:
+        if this.general_info_left_image.name != instance.general_info_left_image.name:
+            print "I am in try"
+            this.general_info_left_image.delete(save=False)                
+    except:
+        print "I am in except"
+        pass # when new photo then we do nothing, normal case          
+    try:
+        if this.roads_and_access_info_left_image.name != instance.roads_and_access_info_left_image.name:
+            print "I am in try"
+            this.roads_and_access_info_left_image.delete(save=False)                
+    except:
+        print "I am in except"
+        pass # when new photo then we do nothing, normal case          
+    try:
+        if this.drainage_info_left_image.name != instance.drainage_info_left_image.name:
+            print "I am in try"
+            this.drainage_info_left_image.delete(save=False)               
+    except:
+        print "I am in except"
+        pass # when new photo then we do nothing, normal case          
+            
+ """ 
