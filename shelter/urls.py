3faa1f662ce64f29f247c76458126c854dac107f
amarkamthe <amarkamthe123@gmail.com>
2016-08-25 12:32:21 +0000

    Added static and media url

--- shelter/urls.py
+++ shelter/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-
+from django.conf import settings
+from django.conf.urls.static import static
 admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include('master.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
