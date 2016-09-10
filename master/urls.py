df2db71425fa5d806ec9effe1f92832679c4531e
PratikDhanave <i.pratikdhanave@gmail.com>
2016-09-11 00:38:23 +0530

    update on rapid_slum_appraisal

--- master/urls.py
+++ master/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic.base import View
 from master.views import index, SurveyListView, SurveyCreateView, \
-    survey_delete_view, search, edit, display, insert, report, AdministrativewardList, ElectoralWardList, SlumList, ReportGenerate, VulnerabilityReport
+    survey_delete_view, search, edit, display, insert, report, AdministrativewardList, ElectoralWardList, SlumList, ReportGenerate, VulnerabilityReport,jsondata
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -50,4 +50,5 @@ urlpatterns = [
     url(r'^SlumList/$',SlumList, name='SlumList'),
     url(r'^ReportGenerate/$',ReportGenerate, name='ReportGenerate'),
     url(r'^VulnerabilityReport/$',VulnerabilityReport, name='VulnerabilityReport'),
+    url(r'^jsondata/$',jsondata, name='jsondata'),
 ]
