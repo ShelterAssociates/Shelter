4d8c564cfdd73c9e9e5aa6135c7fb10843af3724
Sitaram <sitaramsawant2712@gmail.com>
2017-01-13 17:06:12 +0530

    Slum map filter and component

--- component/urls.py
+++ component/urls.py
@@ -6,8 +6,8 @@ from . import views
 urlpatterns = [
     url(r'^$', views.kml_upload, name='kml_upload'),
     url(r'^get_component/(?P<slum_id>\d+)$', views.get_component, name='get_component'),
-    url(r'^get_kobo_data/(?P<slum_id>\d+)$', views.get_kobo_data, name='get_kobo_data'),
-    url(r'^get_kobo_FF_data/(?P<slum_id>\d+)$', views.get_kobo_FF_data, name='get_kobo_FF_data'),
+    url(r'^get_kobo_RIM_data/(?P<slum_id>\d+)$', views.get_kobo_RIM_data, name='get_kobo_RIM_data'),
+    url(r'^get_kobo_FF_data/(?P<slum_id>\d+)/(?P<house_num>\d+)$', views.get_kobo_FF_data, name='get_kobo_FF_data'),
 
 
 ]
