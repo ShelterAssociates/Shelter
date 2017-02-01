4f881d6b7bb58a17dea7573e16d2ab1f0b8b985e
amarkamthe <amarkamthe123@gmail.com>
2017-02-01 13:25:15 +0000

    Made changes to use environment

--- deploy/shelter.sh
+++ deploy/shelter.sh
@@ -7,6 +7,7 @@ NUM_WORKERS=3
 USER=ubuntu
 GROUP=ubuntu
 cd /srv/Shelter/
-source ENV/bin/activate
+source /srv/Shelter/ENV/bin/activate
+#pip freeze
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec gunicorn -b 0.0.0.0:8011 -w $NUM_WORKERS --timeout 600 --user=$USER --group=$GROUP --log-level=info --log-file=$LOGFILE 2>>$LOGFILE shelter.wsgi:application
+exec /srv/Shelter/ENV/bin/gunicorn -b 0.0.0.0:8011 -w $NUM_WORKERS --timeout 600 --user=$USER --group=$GROUP --log-level=info --log-file=$LOGFILE 2>>$LOGFILE shelter.wsgi:application
