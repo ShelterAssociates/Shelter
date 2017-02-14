02268035496208e30e517ba0ac8c440f8a3f181b
paragf <paragf2005@gmail.com>
2017-02-14 17:27:34 +0530

    Made changes to upload photos along with xml files to KoboToolBox

--- scripts/old_data_migration_to_xml/kobotoolbox_upload.py
+++ scripts/old_data_migration_to_xml/kobotoolbox_upload.py
@@ -5,6 +5,7 @@
 
 import traceback
 from multiprocessing import Pool
+import http.client
 
 from common import *
 
@@ -22,7 +23,7 @@ process_file_count = 0
 success_file_upload_count = 0
 fail_file_upload_count = 0
 
-def upload_xml(xml_file):
+def upload_xml(upload_option):
 	
 	global xml_upload_details
 	
@@ -36,37 +37,48 @@ def upload_xml(xml_file):
 	
 	response_obj = None
 	
+	xml_file = upload_option['xml_file']
+	photo_dict = upload_option['xml_photo']
+	
 	try:
 		xml_upload_folder_path = os.path.dirname(xml_file)
 		xml_file_name = os.path.basename(xml_file)
 		
-		#print(process_file_count, ' path -> ', xml_upload_folder_path, '  file => ',xml_file_name)
-		#print(' url -> ', kobotoolbox_url, '  user => ', kobotoolbox_user, '  password => ', kobotoolbox_password)
+		# set xml file to upload
+		upload_files = {
+			'xml_submission_file': (xml_file, open(xml_file, 'rb'), 'text/xml')
+		}
 		
-		upload_file_handler = open(xml_file, 'rb')
-			
+		# add photos if exists for xml to upload
+		if photo_dict:
+			for key, val in photo_dict.items():
+				file_ext = os.path.splitext(xml_file)[1].lower()
+				upload_files[key] = (val, open(val, 'rb'), 'image')
+		
 		process_file_count += 1
 		#print(process_file_count)
 		
-		response_obj = requests.post(kobotoolbox_url, auth=(kobotoolbox_user, kobotoolbox_password), files={"xml_submission_file": upload_file_handler})
+		#print(process_file_count, '  file => ',xml_file_name, '  upload_files => ',upload_files)
 		
-		#print(xml_file_name, ' response_obj.response_obj.status_code -> ', response_obj.status_code, ' requests.codes.ok -> ', requests.codes.ok)
+		response_obj = requests.post(kobotoolbox_url, auth=(kobotoolbox_user, kobotoolbox_password), files = upload_files)
+		
+		#print('after process ', xml_file_name, ' response_obj.response_obj.status_code -> ', response_obj.status_code, ' response_obj.text -> ', response_obj.text)
+		#write_log(xml_upload_folder_path +' \t\t status code= ' + str(response_obj.status_code) +' \t\t\t response text= ' + response_obj.text)
 		
 		if response_obj.status_code == 201: #success status
 			success_file_upload_count += 1
 		else:
 			fail_file_upload_count += 1
 			
-			write_log(xml_upload_folder_path +' \t\t' + xml_file +' \t\t' + (str(response_obj.status_code) if response_obj.status_code else "") +' \t\t\t' + (response_obj.text if response_obj.text else ""))
+			write_log(xml_upload_folder_path +' \t\t status code= ' + str(response_obj.status_code) +' \t\t\t response text= ' + response_obj.text)
 		
-		upload_file_handler.close()
 	except Exception as ex:
 		exception_log = 'Exception occurred for uploading xml file \t  exception : '+ str(ex) +' \t  traceback : '+ traceback.format_exc()
 		write_log(exception_log)
 	
 	return response_obj
 
-def upload_to_kobotoolbox(url, user, password):
+def upload_to_kobotoolbox(survey_photo_mapping, select_option):
 	
 	global process_file_count
 	global success_file_upload_count
@@ -74,33 +86,40 @@ def upload_to_kobotoolbox(url, user, password):
 	
 	xml_root_path = get_survey_option_output_path()
 	
-	xml_file_list = []
+	upload_option = []
+	
+	xml_photo_element_name = survey_photo_mapping[select_option['survey_type']]
 	
 	# get list of xml files to upload with root folder path
 	for dirpath, dirs, files in os.walk(xml_root_path):
 		for filename in files:
 			if filename.endswith('.xml'):
-				xml_file_list.append(os.path.join(dirpath, filename))
-
+				upload_option.append({
+					'xml_file': os.path.join(dirpath, filename),
+					'xml_photo': get_xml_photo_value(dirpath, filename, xml_photo_element_name),
+				})
 	
-	upload_file_count = len(xml_file_list)
+	upload_file_count = len(upload_option)
 	
 	process_file_count = 0
 	success_file_upload_count = 0
 	fail_file_upload_count = 0
 	
-	#print('xml_file_list => ', xml_file_list)
-	
+	#print('upload_option => ', upload_option)
+	print('Start Uploading xml files to KoboToolBox')
 	write_log('Start Uploading xml files to KoboToolBox')
+	
 	
 	# use threading to upload file faster
 	pool_obj = Pool(processes=5)
 	
-	response_obj = pool_obj.map(upload_xml, xml_file_list)
+	response_obj = pool_obj.map(upload_xml, upload_option)
 	
 	pool_obj.close()
 	pool_obj.join()
 	
+	
+	print('Finish upload to KoboToolBox')
 	write_log('Finish upload to KoboToolBox')
 	#result_log = 'Total xml files to upload : '+str(upload_file_count) + ' \t process files : '+str(total_process)  + ' \t total success : '+str(total_success) + ' \t fail to upload : '+str(total_fail)
 	#print(result_log)
