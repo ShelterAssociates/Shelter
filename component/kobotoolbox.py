425bd16eaaee6a048911a8c22d44aaf08dc1801b
Amar Kamthe <amar.kamthe@soft-corner.com>
2017-02-22 22:58:56 +0530

    Code change to maintain order of the RHS and RIM form

--- component/kobotoolbox.py
+++ component/kobotoolbox.py
@@ -53,13 +53,8 @@ def get_kobo_RHS_list(slum_code,house_number):
     req = urllib2.Request(url)
     req.add_header('Authorization', settings.KOBOCAT_TOKEN)
     resp = urllib2.urlopen(req)
-
     content = resp.read()
-    data = json.loads(content)
-
-    for val in data:
-        for k,v in val.items():
-            temp_arr[k]= v
+    submission = json.loads(content)
 
     url1 = settings.KOBOCAT_FORM_URL+'forms/'+settings.KOBOCAT_RHS_SURVEY+'/form.json'
     req1 = urllib2.Request(url1)
@@ -67,28 +62,17 @@ def get_kobo_RHS_list(slum_code,house_number):
     resp1 = urllib2.urlopen(req1)
     content1 = resp1.read()
     data1 = json.loads(content1)
-    values=dictdata(data1)
-    ans=""
-    for k1,v1 in temp_arr.items():
-        if 'group' in k1:
-            c_split=k1.split('/')
-            maindata=values.copy()
-            for n in c_split:
-                maindata=maindata['children'][n]
-            try:
-                ans=""
-                if 'select' in maindata['type']:
-                    for vall in v1.split():
-                        ans+=str(maindata['children'][vall]['label']) +', '
-                    ans = ans[:-2]
-                else:
-                    ans=v1
-            except:
-                ans=""
-                pass
 
-        if ans != "":
-            output[maindata['label']]= ans
+    output = OrderedDict()
+    for data in data1['children']:
+        if data['type'] == "group":
+            sect_form_data = trav(data)
+            sub_key = [ str(k) for k in submission[0].keys() if data['name'] in k]
+            for sect_form in sect_form_data:
+                key = [x for x in sub_key if sect_form['name'] in x]
+                if len(key)>0 and 'label' in sect_form:
+                    ans = fetch_answer(sect_form, key, submission[0])
+                    output[sect_form['label']]  = ans
     return output
 
 
@@ -99,16 +83,8 @@ def get_kobo_RIM_detail(slum_code):
     req.add_header('Authorization', settings.KOBOCAT_TOKEN)
     resp = urllib2.urlopen(req)
     content = resp.read()
-    data = json.loads(content)
+    submission = json.loads(content)
 
-    temp_arr = OrderedDict()
-    temp_arr['General']=OrderedDict()
-    temp_arr['Toilet']=OrderedDict()
-    temp_arr['Water']=OrderedDict()
-    temp_arr['Waste']=OrderedDict()
-    temp_arr['Drainage']=OrderedDict()
-    temp_arr['Gutter']=OrderedDict()
-    temp_arr['Road']=OrderedDict()
     output=OrderedDict()
 
     RIM_GENERAL="group_zl6oo94"
@@ -118,24 +94,8 @@ def get_kobo_RIM_detail(slum_code):
     RIM_DRAINAGE="group_kk5gz02"
     RIM_GUTTER="group_bv7hf31"
     RIM_ROAD="group_xy9hz30"
-
-    for val in data:
-        for k,v in val.items():
-            if RIM_GENERAL==k.split('/')[0]:
-             temp_arr['General'][k]=v
-            elif RIM_TOILET==k.split('/')[0]:
-             temp_arr['Toilet'][k]=v
-            elif RIM_WATER==k.split('/')[0]:
-             temp_arr['Water'][k]=v
-            elif RIM_WASTE==k.split('/')[0]:
-             temp_arr['Waste'][k]=v
-            elif RIM_DRAINAGE==k.split('/')[0]:
-             temp_arr['Drainage'][k]=v
-            elif RIM_GUTTER==k.split('/')[0]:
-             temp_arr['Gutter'][k]=v
-            elif RIM_ROAD==k.split('/')[0]:
-             temp_arr['Road'][k]=v
-
+    section = {RIM_GENERAL:"General", RIM_TOILET:"Toilet", RIM_WATER:"Water",
+               RIM_WASTE:"Waste", RIM_DRAINAGE:"Drainage", RIM_GUTTER:"Gutter", RIM_ROAD:"Road"}
 
     url1 = settings.KOBOCAT_FORM_URL+'forms/'+settings.KOBOCAT_RIM_SURVEY+'/form.json'
     req1 = urllib2.Request(url1)
@@ -143,63 +103,70 @@ def get_kobo_RIM_detail(slum_code):
     resp1 = urllib2.urlopen(req1)
     content1 = resp1.read()
     data1 = json.loads(content1)
-    values=dictdata(data1)
-
-    #*****************************************/
-
-    ans=""
-    for key,section_list in temp_arr.items():
-        output[key]={}
-        for k1,v1 in section_list.items():
-            if 'group' in k1:
-                c_split=k1.split('/')
-                maindata=values.copy()
-                for n in c_split:
-                    maindata=maindata['children'][n]
-
-                try:
-                    ans=""
-                    que=""
-                    if 'select' in maindata['type']:
-                        for vall in v1.split():
-                            ans+=str(maindata['children'][vall]['label']) +', '
-                        ans = ans[:-2]
-                        que= maindata['label']
-                    elif 'repeat' in maindata['type']:
-                        que=[]
-
-                        for val1 in v1:
-                            que_dict={}
-                            for kn1,vn1 in val1.items():
-                                arr_kn1=kn1.split('/')
-                                if 'select' in maindata['children'][arr_kn1[len(arr_kn1)-1]]['type']:
-                                    ans=""
-                                    for vall in vn1.split():
-                                        ans+=str(maindata['children'][arr_kn1[len(arr_kn1)-1]]['children'][vall]['label']) +', '
-                                    vn1 = ans[:-2]
-                                    que_dict[maindata['children'][arr_kn1[len(arr_kn1)-1]]['label']]= vn1
-                                else:
-                                    que_dict[maindata['children'][arr_kn1[len(arr_kn1)-1]]['label']]= vn1
-
-                            que.append(que_dict)
-
+    #To maintain the order in which questions are displayed we iterate through the form data
+    for data in data1['children']:
+        if data['type'] == "group" and data['name'] in section.keys():
+            #Group wise get the entire list for questions
+            sect_form_data = trav(data)
+            #Find the list of keys available in the submission data
+            toil_keys = [ str(k) for k in submission[0].keys() if data['name'] in k]
+            count = 1
+            sub_key = []
+            sub = []
+            # Needed for toilet section which has repeat section
+            for sub_k in toil_keys:
+                if type(submission[0][sub_k]) == list:
+                    count = len(submission[0][sub_k])
+                    sub = submission[0][sub_k]
+                    sub_key.extend(sum([k.keys() for k in submission[0][sub_k]], []))
+                else:
+                    sub_key.append(sub_k)
+            #Default values
+            if data['name'] != RIM_TOILET:
+                output[data['label']] = OrderedDict()
+            else:
+                output[data['label']] = []
+                [output[data['label']].append(OrderedDict()) for i in range(count)]
+            #Iterate through the list of questions for the group
+            for sect_form in sect_form_data:
+                key = [x for x in sub_key if sect_form['name'] in x]
+                #Check if the question has answer in the submission then only proceed further
+                if len(key)>0 and 'label' in sect_form:
+                    if data['name'] != RIM_TOILET:
+                        #Fetch the answer for select one/text/select multiple type question
+                        ans = fetch_answer(sect_form, key, submission[0])
+                        output[data['label']][sect_form['label']]  = ans
                     else:
-                        ans=v1
-                        que= maindata['label']
-                    if type(que)==list:
-                        output[key]=que
-                    else:
-                        output[key][que]=ans
-                except:
-                    ans=""
-                    pass
+                        #For toilet repeative section append the set of questions for all the CTB's if available
+                        for ind in range(count):
+                            if key[0] in sub[ind].keys():
+                                ans = fetch_answer(sect_form, key, sub[ind])
+                                output[section[data['name']]][ind][sect_form['label']] = ans
 
     return output
 
+def fetch_answer(sect_form, key, submission):
+    #Fetch answer and convert it to label
+    val = ""
+    if 'select' in sect_form['type'] and type(key[0]) != list and 'children' in sect_form:
+        options = dict((opt['name'], opt['label']) for opt in sect_form['children'])
+        sub_option = submission[key[0]].split(' ')
+        val = options[sub_option[0]]
+        if len(sub_option) > 1:
+            val = reduce(lambda x,y: options[x] +',' + options[y], sub_option)
+    else:
+        val = submission[key[0]]
+    return val
 
+def trav(node):
+    #Traverse uptill the child node and add to list
+    if 'type' in node and node['type'] == "group" or node['type'] == "repeat":
+        return list(chain.from_iterable([trav(child) for child in node['children']]))
+    else:
+        return [node]
 
 def dictdata(data):
-
+    #convert childrens to dictionary for parsing
     data['children'] = dict((str(topic['name']), topic) for topic in data['children'])
 
     for k,v in data['children'].items():
