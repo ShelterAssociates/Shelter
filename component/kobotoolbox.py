07b5ba553fbc90f8b1d573bf0a0a9da56c560ab2
Amar Kamthe <amar.kamthe@soft-corner.com>
2017-02-24 16:58:10 +0530

    Code change to dynamically select form

--- component/kobotoolbox.py
+++ component/kobotoolbox.py
@@ -4,6 +4,22 @@ from django.conf import settings
 import copy
 from collections import OrderedDict
 from itertools import chain
+from master.models import SURVEYTYPE_CHOICES, Survey
+
+def survey_mapping(type):
+    def real_decorator(function):
+        def wrapper(*args, **kwargs):
+            city_id = args['city']
+            try:
+                survey = Survey.objects.filter(city__id=city_id, survey_type=type)
+            except:
+                survey = None
+            kwargs['kobo_survey'] = ''
+            if survey:
+                kwargs['kobo_survey'] =  survey.kobotool_survey_id
+            function(*args, **kwargs)
+        return wrapper
+    return real_decorator
 
 def get_household_analysis_data(slum_code, fields):
     '''Gets the kobotoolbox RHS data for selected questions
@@ -76,73 +92,74 @@ def get_kobo_RHS_list(slum_code,house_number):
                     output[sect_form['label']]  = ans
     return output
 
-
-def get_kobo_RIM_detail(slum_code):
-    url = settings.KOBOCAT_FORM_URL+'data/'+settings.KOBOCAT_RIM_SURVEY+'?query={"group_zl6oo94/group_uj8eg07/slum_name":"'+slum_code+'"}'
-
-    req = urllib2.Request(url)
-    req.add_header('Authorization', settings.KOBOCAT_TOKEN)
-    resp = urllib2.urlopen(req)
-    content = resp.read()
-    submission = json.loads(content)
+@survey_mapping(SURVEYTYPE_CHOICES['Slum Level'])
+def get_kobo_RIM_detail(city, slum_code, kobo_survey=''):
 
     output=OrderedDict()
+    if kobo_survey:
+        url = settings.KOBOCAT_FORM_URL+'data/'+kobo_survey+'?query={"group_zl6oo94/group_uj8eg07/slum_name":"'+slum_code+'"}'
 
-    RIM_GENERAL="group_zl6oo94"
-    RIM_TOILET="group_te3dx03"
-    RIM_WATER="group_zj8tc43"
-    RIM_WASTE="group_ks0wh10"
-    RIM_DRAINAGE="group_kk5gz02"
-    RIM_GUTTER="group_bv7hf31"
-    RIM_ROAD="group_xy9hz30"
-    section = {RIM_GENERAL:"General", RIM_TOILET:"Toilet", RIM_WATER:"Water",
-               RIM_WASTE:"Waste", RIM_DRAINAGE:"Drainage", RIM_GUTTER:"Gutter", RIM_ROAD:"Road"}
+        req = urllib2.Request(url)
+        req.add_header('Authorization', settings.KOBOCAT_TOKEN)
+        resp = urllib2.urlopen(req)
+        content = resp.read()
+        submission = json.loads(content)
 
-    url1 = settings.KOBOCAT_FORM_URL+'forms/'+settings.KOBOCAT_RIM_SURVEY+'/form.json'
-    req1 = urllib2.Request(url1)
-    req1.add_header('Authorization', settings.KOBOCAT_TOKEN)
-    resp1 = urllib2.urlopen(req1)
-    content1 = resp1.read()
-    data1 = json.loads(content1)
-    #To maintain the order in which questions are displayed we iterate through the form data
-    for data in data1['children']:
-        if data['type'] == "group" and data['name'] in section.keys():
-            #Group wise get the entire list for questions
-            sect_form_data = trav(data)
-            #Find the list of keys available in the submission data
-            toil_keys = [ str(k) for k in submission[0].keys() if data['name'] in k]
-            count = 1
-            sub_key = []
-            sub = []
-            # Needed for toilet section which has repeat section
-            for sub_k in toil_keys:
-                if type(submission[0][sub_k]) == list:
-                    count = len(submission[0][sub_k])
-                    sub = submission[0][sub_k]
-                    sub_key.extend(sum([k.keys() for k in submission[0][sub_k]], []))
-                else:
-                    sub_key.append(sub_k)
-            #Default values
-            if data['name'] != RIM_TOILET:
-                output[section[data['name']]] = OrderedDict()
-            else:
-                output[section[data['name']]] = []
-                [output[section[data['name']]].append(OrderedDict()) for i in range(count)]
-            #Iterate through the list of questions for the group
-            for sect_form in sect_form_data:
-                key = [x for x in sub_key if sect_form['name'] in x]
-                #Check if the question has answer in the submission then only proceed further
-                if len(key)>0 and 'label' in sect_form:
-                    if data['name'] != RIM_TOILET:
-                        #Fetch the answer for select one/text/select multiple type question
-                        ans = fetch_answer(sect_form, key, submission[0])
-                        output[section[data['name']]][sect_form['label']]  = ans
+        RIM_GENERAL="group_zl6oo94"
+        RIM_TOILET="group_te3dx03"
+        RIM_WATER="group_zj8tc43"
+        RIM_WASTE="group_ks0wh10"
+        RIM_DRAINAGE="group_kk5gz02"
+        RIM_GUTTER="group_bv7hf31"
+        RIM_ROAD="group_xy9hz30"
+        section = {RIM_GENERAL:"General", RIM_TOILET:"Toilet", RIM_WATER:"Water",
+                   RIM_WASTE:"Waste", RIM_DRAINAGE:"Drainage", RIM_GUTTER:"Gutter", RIM_ROAD:"Road"}
+
+        url1 = settings.KOBOCAT_FORM_URL+'forms/'+kobo_survey+'/form.json'
+        req1 = urllib2.Request(url1)
+        req1.add_header('Authorization', settings.KOBOCAT_TOKEN)
+        resp1 = urllib2.urlopen(req1)
+        content1 = resp1.read()
+        data1 = json.loads(content1)
+        #To maintain the order in which questions are displayed we iterate through the form data
+        for data in data1['children']:
+            if data['type'] == "group" and data['name'] in section.keys():
+                #Group wise get the entire list for questions
+                sect_form_data = trav(data)
+                #Find the list of keys available in the submission data
+                toil_keys = [ str(k) for k in submission[0].keys() if data['name'] in k]
+                count = 1
+                sub_key = []
+                sub = []
+                # Needed for toilet section which has repeat section
+                for sub_k in toil_keys:
+                    if type(submission[0][sub_k]) == list:
+                        count = len(submission[0][sub_k])
+                        sub = submission[0][sub_k]
+                        sub_key.extend(sum([k.keys() for k in submission[0][sub_k]], []))
                     else:
-                        #For toilet repeative section append the set of questions for all the CTB's if available
-                        for ind in range(count):
-                            if key[0] in sub[ind].keys():
-                                ans = fetch_answer(sect_form, key, sub[ind])
-                                output[section[data['name']]][ind][sect_form['label']] = ans
+                        sub_key.append(sub_k)
+                #Default values
+                if data['name'] != RIM_TOILET:
+                    output[section[data['name']]] = OrderedDict()
+                else:
+                    output[section[data['name']]] = []
+                    [output[section[data['name']]].append(OrderedDict()) for i in range(count)]
+                #Iterate through the list of questions for the group
+                for sect_form in sect_form_data:
+                    key = [x for x in sub_key if sect_form['name'] in x]
+                    #Check if the question has answer in the submission then only proceed further
+                    if len(key)>0 and 'label' in sect_form:
+                        if data['name'] != RIM_TOILET:
+                            #Fetch the answer for select one/text/select multiple type question
+                            ans = fetch_answer(sect_form, key, submission[0])
+                            output[section[data['name']]][sect_form['label']]  = ans
+                        else:
+                            #For toilet repeative section append the set of questions for all the CTB's if available
+                            for ind in range(count):
+                                if key[0] in sub[ind].keys():
+                                    ans = fetch_answer(sect_form, key, sub[ind])
+                                    output[section[data['name']]][ind][sect_form['label']] = ans
 
     return output
 
