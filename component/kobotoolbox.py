a9377e08ed31385eaa669bf4297cb8e6b3d59ff0
Amar Kamthe <amar.kamthe@soft-corner.com>
2017-02-24 18:20:06 +0530

    Added changes for RHS analysis to dynamically fetch data

--- component/kobotoolbox.py
+++ component/kobotoolbox.py
@@ -21,38 +21,40 @@ def survey_mapping(survey_type):
         return wrapper
     return real_decorator
 
-def get_household_analysis_data(slum_code, fields):
+@survey_mapping(SURVEYTYPE_CHOICES[1][0])
+def get_household_analysis_data(city, slum_code, fields, kobo_survey=''):
     '''Gets the kobotoolbox RHS data for selected questions
     '''
     household_field = 'group_ce0hf58/house_no'
-
-    #Setting up API call and header data
-    url = settings.KOBOCAT_FORM_URL + 'data/'+ settings.KOBOCAT_RHS_SURVEY +'?query={"group_ce0hf58/slum_name":"'+slum_code+'"}&fields="'+ str(fields + [household_field] )+ '"'
-
-    kobotoolbox_request = urllib2.Request(url)
-    kobotoolbox_request.add_header('User-agent', 'Mozilla 5.10')
-    kobotoolbox_request.add_header('Authorization', settings.KOBOCAT_TOKEN)
-
-    res = urllib2.urlopen(kobotoolbox_request)
-    #Read json data from kobotoolbox API
-    html = res.read()
-    records = json.loads(html)
-
     output = {}
-    #Process received data
-    for record in records:
-        household_no = record[household_field]
-        for field in fields:
-            if field != "" and field in record:
 
-                data = record[field]
-                for val in data.split():
-                    if field not in output:
-                        output[field] = {}
-                    if data not in output[field]:
-                        output[field][data]=[]
-                    if household_no not in output[field][data]:
-                        output[field][data].append(household_no)
+    if kobo_survey:
+        #Setting up API call and header data
+        url = settings.KOBOCAT_FORM_URL + 'data/'+ kobo_survey +'?query={"group_ce0hf58/slum_name":"'+slum_code+'"}&fields="'+ str(fields + [household_field] )+ '"'
+
+        kobotoolbox_request = urllib2.Request(url)
+        kobotoolbox_request.add_header('User-agent', 'Mozilla 5.10')
+        kobotoolbox_request.add_header('Authorization', settings.KOBOCAT_TOKEN)
+
+        res = urllib2.urlopen(kobotoolbox_request)
+        #Read json data from kobotoolbox API
+        html = res.read()
+        records = json.loads(html)
+
+        #Process received data
+        for record in records:
+            household_no = record[household_field]
+            for field in fields:
+                if field != "" and field in record:
+
+                    data = record[field]
+                    for val in data.split():
+                        if field not in output:
+                            output[field] = {}
+                        if data not in output[field]:
+                            output[field][data]=[]
+                        if household_no not in output[field][data]:
+                            output[field][data].append(household_no)
     return output
 
 @survey_mapping(SURVEYTYPE_CHOICES[1][0])
