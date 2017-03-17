a00e306857c932de30c21332dc8054aa05ac5996
Amar Kamthe <amar.kamthe@soft-corner.com>
2017-03-17 11:06:58 +0530

    Sponsor implementation and FF report API integration with BIRT

--- sponsor/admin.py
+++ sponsor/admin.py
@@ -1,21 +1,55 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.admin import UserAdmin
+from django.http import HttpResponseRedirect
 
 admin.site.register(Sponsor)
 admin.site.register(SponsorContact)
-admin.site.register(SponsorProjectDetails)
 
-# Register your models here.
+class SponsorProjectDetailsAdmin(admin.ModelAdmin):
+	list_display = ('sponsor', 'slum', 'project_name', 'household_code')
+	raw_id_fields = ['slum']
+	def slum(self, obj):
+		return obj.slum.name
+
+	def project_name(self, obj):
+		return obj.sponsor_project.name
+
+admin.site.register(SponsorProjectDetails, SponsorProjectDetailsAdmin)
 
 class SponsorProjectAdmin(admin.ModelAdmin):
-	list_display = ( 
-				"type",
-				"sponsor", 
-	)
+	list_display = ("get_project_type","name", "get_status")
 	exclude = ('created_by','created_on')
+
+	def get_project_type(self, obj):
+		return obj.get_project_type_display()
+
+	def get_status(self, obj):
+		return obj.get_status_display()
+
 	def save_model(self, request, obj, form, change):
-		obj.createdBy = request.user
-		obj.save()   
+		obj.created_by = request.user
+		obj.save()
+		#super(SponsorProjectAdmin, self).save_model(request, obj, form, change)
 
 admin.site.register(SponsorProject,SponsorProjectAdmin)
 
+class UserAdminCust(UserAdmin):
+	def save_model(self, request, obj, form, change):
+		super(UserAdminCust, self).save_model(request, obj, form, change)
+		#import pdb; pdb.set_trace()
+		sponsor = Group.objects.filter(name = 'sponsor').first().id
+		group_ids = [int(x) for x in request.POST.getlist('groups')]
+		if sponsor in group_ids:
+			sponsor, created = Sponsor.objects.get_or_create(user=obj, defaults={'user':obj})
+
+	def response_change(self, request, obj):
+		print "Inside change"
+		if obj.groups.filter(name="sponsor").exists():
+			sponsor = Sponsor.objects.get(user = obj)
+            # hardcoded url to be replaced with reverse url
+			return HttpResponseRedirect('../../../sponsor/sponsor/' + str(sponsor.id) + '/' )
+		else:
+			return super(UserAdminCust, self).response_change(request, obj)
+admin.site.unregister(User)
+admin.site.register(User,UserAdminCust)
--- sponsor/models.py
+++ sponsor/models.py
@@ -3,68 +3,84 @@ from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
 from datetime import datetime
 from master.models import Slum
+from jsonfield import JSONField
 
+LOGO_PATH = 'sponsor_logo/'
 class Sponsor(models.Model):
-	organization = models.CharField(max_length=200)
+	organization_name = models.CharField(max_length=1024)
 	address = models.CharField(max_length=2048)
-	website = models.CharField(max_length = 2048)
+	website_link = models.CharField(max_length = 2048, null=True, blank=True)
 	intro_date = models.DateTimeField(default=datetime.now())
-	other_info = models.CharField(max_length=2048)
-	image = models.CharField(max_length=2048)
+	other_info = models.TextField(null=True, blank=True)
+	logo = models.ImageField(upload_to=LOGO_PATH, null=True, blank=True)
+	user = models.ForeignKey(User, limit_choices_to={'groups__name': "sponsor"})
 
-	class Meta: 
-	 	verbose_name = 'Sponsor' 
-	 	verbose_name_plural = 'Sponsors'  
+	class Meta:
+	 	verbose_name = 'Sponsor'
+	 	verbose_name_plural = 'Sponsors'
 
+	def __unicode__(self):
+		"""Returns string representation of object"""
+		return self.organization_name
 
-TYPE_CHOICES = (('0', 'Intervention'),
-					  ('1', 'Collection'))
+TYPE_CHOICES = (('1', 'Intervention'),
+					  ('2', 'Collection'))
 
-PROJECTSTATUS_CHOICES = ( ('0','Planned'),
-							('1','Activated'),
-							('2','Closed')
+PROJECTSTATUS_CHOICES = ( ('1','Planned'),
+							('2','Activated'),
+							('3','Closed')
 	)
 
 class SponsorProject(models.Model):
-	sponsor = models.ForeignKey(Sponsor)
 	name = models.CharField(max_length=512)
-	type =  models.IntegerField(choices=TYPE_CHOICES)
+	project_type =  models.CharField(choices=TYPE_CHOICES, max_length=2)
 	funds_sponsored = models.DecimalField(max_digits=10, decimal_places=2)
-	additional_info = models.CharField(max_length=2048)
-	start_date = models.DateTimeField()
-	end_date = models.DateTimeField()
-	funds_utilised = models.DecimalField(max_digits=10, decimal_places=2)
+	additional_info = models.TextField(null=True, blank=True)
+	start_date = models.DateTimeField(null=True, blank=True)
+	end_date = models.DateTimeField(null=True, blank=True)
+	funds_utilised = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 	status = models.CharField(choices = PROJECTSTATUS_CHOICES, max_length=2)
 	created_by = models.ForeignKey(User)
 	created_on= models.DateTimeField(default= datetime.now())
+	project_details = models.ManyToManyField(Sponsor, through='SponsorProjectDetails')
 
 	def __unicode__(self):
 		return self.name
 
-	class Meta: 
-	 	verbose_name = 'Sponsor Project' 
-	 	verbose_name_plural = 'Sponsor Projects'  
+	class Meta:
+	 	verbose_name = 'Sponsor Project'
+	 	verbose_name_plural = 'Sponsor Projects'
+
 
 class SponsorProjectDetails(models.Model):
-	household_code = models.IntegerField()
-	slum = models.ForeignKey(Slum)
+	sponsor = models.ForeignKey(Sponsor)
 	sponsor_project = models.ForeignKey(SponsorProject)
+	slum = models.ForeignKey(Slum)
+	household_code = JSONField(null=True, blank=True)
 
-	class Meta: 
-	 	verbose_name = 'Sponsor Project Detail' 
-	 	verbose_name_plural = 'Sponsor Project Details'  
+	class Meta:
+		unique_together = ('sponsor', 'sponsor_project', 'slum',)
+	 	verbose_name = 'Sponsor Project Detail'
+	 	verbose_name_plural = 'Sponsor Project Details'
+
+	def __unicode__(self):
+		"""Returns string representation of object"""
+		return self.slum.name
 
 STATUS_CHOICES = (('0','InActive'),
-	('1','Active')
-	)
+	              ('1','Active'))
 
 class SponsorContact(models.Model):
 	sponsor = models.ForeignKey(Sponsor)
 	name = models.CharField(max_length=512)
 	email_id = models.CharField(max_length=512)
-	contact_no = models.CharField(max_length=256)
+	contact_no = models.CharField(max_length=256, null=True, blank=True)
 	status = models.CharField(choices=STATUS_CHOICES, max_length=2)
 
-	class Meta: 
-	 	verbose_name = 'Sponsor Contact' 
-	 	verbose_name_plural = 'Sponsor Contacts'  
+	class Meta:
+	 	verbose_name = 'Sponsor Contact'
+	 	verbose_name_plural = 'Sponsor Contacts'
+
+	def __unicode__(self):
+		"""Returns string representation of object"""
+		return self.name
