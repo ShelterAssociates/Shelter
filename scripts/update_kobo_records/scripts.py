fdff43cf518f7a0742e404031c2e90573d3cb922
Amar Kamthe <amar.kamthe@soft-corner.com>
2017-08-31 15:19:04 +0530

    Resolved folder and api url issue

--- scripts/update_kobo_records/scripts.py
+++ scripts/update_kobo_records/scripts.py
@@ -1,5 +1,9 @@
 ''''
     Script to update PCMC RHS records for current place of defecation. 
+    To run include the folder in sys path
+    
+    import sys
+    sys.path.insert(0, 'scripts/update_kobo_records/')
 '''
 
 import uuid
@@ -26,6 +30,7 @@ kobo_survey = '54'
 root_folder_path = os.path.dirname(os.path.abspath(__file__))
 root_output_folder = os.path.join(root_folder_path, 'xml_output')
 output_folder_path = os.path.join(root_output_folder, 'PCMC', 'RHS')
+
 
 def rhs_form():
     gl_rhs_xml_dict = {
@@ -82,10 +87,11 @@ def rhs_form():
     return gl_rhs_xml_dict
 
 def fetch():
+    print "Start fetching the details"
     sponsored_households = SponsorProjectDetails.objects.filter(sponsor__organization_name = "SBM Toilets")
     for sponsored_household in sponsored_households:
         slum_code = sponsored_household.slum.shelter_slum_code
-        url = settings.KOBOCAT_FORM_URL + 'data/' + kobo_survey + '?query={"group_ce0hf58/slum_name":"' + slum_code + '"}'
+        url = settings.KOBOCAT_FORM_URL + 'data/' + kobo_survey + '?format=json&query={"group_ce0hf58/slum_name":"' + slum_code + '"}'
         kobotoolbox_request = urllib2.Request(url)
         kobotoolbox_request.add_header('User-agent', 'Mozilla 5.10')
         kobotoolbox_request.add_header('Authorization', settings.KOBOCAT_TOKEN)
@@ -98,6 +104,7 @@ def fetch():
             records[list_record[0]] = list(list_record[1])
         slum = sponsored_household.slum.name
         print slum
+        folder_path = os.path.join(output_folder_path, "slum_" + str(slum.replace('/','')))
         rec = []
         for house in sponsored_household.household_code:
             #house = '000'.join(str(house))[-4:]
@@ -105,24 +112,26 @@ def fetch():
                 record_sorted = sorted(records[house], key=lambda x: x['end'], reverse=True)
                 record = record_sorted[0]
                 if 'group_ye18c77/group_yw8pj39/Current_place_of_defecation_toilet' in record and record['group_ye18c77/group_yw8pj39/Current_place_of_defecation_toilet'] in ['01', '02']:
-                    rec.append(str(house) +' :: '+ str(len(record_sorted)) + ' :: '+str(record['_id']))
+                    #rec.append(str(house) +' :: '+ str(len(record_sorted)) + ' :: '+str(record['_id']))
                     create_data = pcmc_rhs_xml_create(record)
+
                     status, replace_data = pcmc_rhs_xml_replace(create_data)
                     if status:
                         replace_data['__version__'] = record['__version__']
                         xml_string = create_xml_string(replace_data, [], record['_xform_id_string'], record['_xform_id_string'], record['__version__'])
-                        file_name = 'RHS_Survey_Slum_Id_' + str(slum) + '_House_code_' + str(house)
-                        folder_path = os.path.join(output_folder_path, "slum_" + str(slum))
+                        file_name = 'RHS_Survey_Slum_Id_' + str(slum.replace('/','')) + '_House_code_' + str(house)
+
 
                         create_xml_file(xml_string, file_name, folder_path)
             else:
-                print "Not found" + str(house)
+                rec.append(str(house))#print "Not found" + str(house)
         print rec
 
 
-def pcmc_rhs_xml_create(record):
+def pcmc_rhs_xml_create(xml_record):
     gl_rhs_xml_dict = rhs_form()
-    for key, val in enumerate(record):
+    for key, val in xml_record.iteritems():
+        key = str(key)
         if not key.startswith( '_' ):
             split_key = key.split('/')
             if len(split_key) == 1:
@@ -136,17 +145,17 @@ def pcmc_rhs_xml_create(record):
 
     return gl_rhs_xml_dict
 
-def pcmc_rhs_xml_replace(record):
+def pcmc_rhs_xml_replace(rhs_record):
     flag=False
-    if record['group_ce0hf58']['Type_of_structure_occupancy'] == '01' :
-        record['group_ye18c77']['group_yw8pj39']['Current_place_of_defecation_toilet'] = "03"
-        record['meta']['deprecatedID'] = record['meta']['instanceID']
-        record['meta']['instanceID'] = 'uuid:' + str(uuid.uuid4())
+    if rhs_record['group_ce0hf58']['Type_of_structure_occupancy'] == '01' :
+        rhs_record['group_ye18c77']['group_yw8pj39']['Current_place_of_defecation_toilet'] = "03"
+        rhs_record['meta']['deprecatedID'] = rhs_record['meta']['instanceID']
+        rhs_record['meta']['instanceID'] = 'uuid:' + str(uuid.uuid4())
         flag = True
     else:
         flag = False
         print "ERROR"
-    return flag, record
+    return flag, rhs_record
 
 
 def create_xml_string(xml_dict, repeat_dict, xml_root, xml_root_attr_id, xml_root_attr_version):
