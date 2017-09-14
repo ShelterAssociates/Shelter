0d1753b55955581283ad0fde26512726f3d77c81
Amar Kamthe <amar.kamthe@soft-corner.com>
2017-09-14 12:14:36 +0530

    Added multiprocessing to speed up the task

--- scripts/delete_kobo_records/scripts.py
+++ scripts/delete_kobo_records/scripts.py
@@ -9,19 +9,35 @@
 from django.conf import settings
 import json
 import requests
+from multiprocessing import Pool
+kobo_form = 41
+headers={}
+headers["Authorization"] = settings.KOBOCAT_TOKEN
 
-def delete_records(kobo_form):
-    headers={}
-    headers["Authorization"] = settings.KOBOCAT_TOKEN
+def fetch_records(formID):
+    global kobo_form
+    if formID:
+        kobo_form = formID
     fetchURL = '/'.join([settings.KOBOCAT_FORM_URL[:-1], 'data', str(kobo_form)])
     objresponse = requests.get(fetchURL + '?format=json&fields=["_id"]', headers=headers)
     records = objresponse.json()
     print (records)
     print ("Total number of records to be deleted : " + str(len(records)))
-    count = 0
-    for record in records:
+
+    pool_obj = Pool(processes=5)
+    response_obj = pool_obj.map(delete_record, records)
+    print (response_obj)
+    pool_obj.close()
+    pool_obj.join()
+
+# Creating subprocess
+def delete_record(records):
+    objresponseDeleted=None
+    record = records
+    if record:
         delete_record_id = record['_id']
         deleteURL = '/'.join([settings.KOBOCAT_FORM_URL[:-1],'data', str(kobo_form) , str(delete_record_id)])
-        #objresponseDeleted = requests.delete(deleteURL, headers=headers)
-        count += 1
-        print(str(count) + ' deleted for '+str(delete_record_id) + ' with response '#+objresponseDeleted)
+        objresponseDeleted = requests.delete(deleteURL, headers=headers)
+
+        print(' deleted for '+str(delete_record_id) + ' with response '+str(objresponseDeleted))
+    return objresponseDeleted
