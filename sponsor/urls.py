1618ff22cd07696f2b822f51379ea1f8a6fb43bd
Shelter <gis@gmail.com>
2017-10-11 12:11:45 +0530

    Changes for log in page of sponsor are made.New feature of PDF of households in zip format is initiated, but not entirely completed.

--- sponsor/urls.py
+++ sponsor/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 
-from sponsor.views import sponsors
+from sponsor.views import *
 
 urlpatterns = [
+
+	url(r'^(?P<slumname>.*)/$', create_zip, name = 'create_zip'),
     url(r'^$', sponsors, name='sponsors'),
+   
 ]
--- sponsor/views.py
+++ sponsor/views.py
@@ -3,6 +3,10 @@ from sponsor.models import *
 from master.models import *	
 from django.views.decorators.csrf import csrf_exempt
 import json
+from component.cipher import *
+import commands
+import zipfile
+import os
 
 """
 def sponsors(request):
@@ -68,20 +72,23 @@ def sponsors(request):
 
 
 def sponsors(request):
-	print "user: " + str(request.user)
+	#print "user: " + str(request.user)
+	print "Sponsors madhe aala"
 	data=request.user
 	Sp = Sponsor.objects.filter(user=data)
 	k = 0
+	#print "Sp = " + str(Sp)
 	for i in Sp:
 		k = i.id
-	print "k=" + str(k)	
+	#print "k=" + str(k)	
 	sponsor_projectArray = []
 	Spd=SponsorProjectDetails.objects.filter(sponsor=k)
 	for i in Spd:##print i.sponsor_project.name#print i.slum.nam#print i.slum.id
 	    sponsor_projectArray.append(str(i.sponsor_project.name))
 	sponsor_projectArray = sorted(set(sponsor_projectArray))
-	print sponsor_projectArray
+	#print sponsor_projectArray
 	dataarray =[]
+	cipher = AESCipher()
 	slumnameArray = []    	
 	for i in sponsor_projectArray:
 		data = {}
@@ -90,24 +97,55 @@ def sponsors(request):
 			slumdict={}
 			project_type_index=j.sponsor_project.project_type
 			TYPE_CHOICESdict=dict(TYPE_CHOICES)
-			print TYPE_CHOICESdict[project_type_index]
-			print j.slum.name
-			print j.slum.electoral_ward.administrative_ward.city
-			print j.slum.electoral_ward.administrative_ward.city.id
 			cref=CityReference.objects.get(id=j.slum.electoral_ward.administrative_ward.city.name_id)
-			print cref.city_name
 			slumdict.update({'slumname':j.slum.name,
-							 'slum_id':j.slum.id,'count':len(j.household_code) ,
-							 'cityname' : str(cref.city_name)
-							 ,'city_id' : j.slum.electoral_ward.administrative_ward.city.id,	
+							 'slum_id':j.slum.id,'count':len(j.household_code),
+							 'cityname' : str(cref.city_name),
+							 'city_id' : j.slum.electoral_ward.administrative_ward.city.id,	
+							 'city_id_encrypted' : "city::" + cipher.encrypt(str(j.slum.electoral_ward.administrative_ward.city.id)),
 							 'project_type': TYPE_CHOICESdict[project_type_index]
 							 })
-			print slumdict
+			#print slumdict
 			slumnameArray.append(slumdict)
 		data.update({'sponsor_project_name':i,'slumnames':slumnameArray})
 		dataarray.append(data)
-	print ("dataarray",":", dataarray)
-	print ("slumnameArray:",":",slumnameArray)  	    
+	#print ("dataarray",":", dataarray)
+	#print ("slumnameArray:",":",slumnameArray)  	    
 	jsondata ={}
 	jsondata= { 'dataarray'  : dataarray}
 	return render(request, 'sponsors.html', {'cities':slumnameArray})
+
+def create_zip(request, slumname):
+
+	print "We are in create_zzip"
+
+	cipher = AESCipher()
+	user_id = request.user.id
+	logged_sponsor = Sponsor.objects.get(user__id = user_id)
+	SlumObj = Slum.objects.get(name = slumname)
+	sponsored_slums = SponsorProjectDetails.objects.filter(slum = SlumObj).filter(sponsor = logged_sponsor)
+	
+	rp_slum_code = str(SlumObj.shelter_slum_code)
+
+
+	folder_name = '/home/shelter/Documents/Project/Shelter/media/'+ str(request.user)
+	os.mkdir(folder_name)
+	print sponsored_slums[0].household_code
+	for household_code in sponsored_slums[0].household_code:
+
+		key = cipher.encrypt(str(rp_slum_code) + '|' + str(household_code) + '|' +  str(request.user.id))
+		com = "sh /home/shelter/BIRT_NEW/ReportEngine/genReport.sh -f PDF -o "+folder_name+"/household_code_"+str(household_code)+".pdf -p key=" + key + " /home/shelter/Documents/Project/Shelter/reports/FFReport.rptdesign"
+		os.system(com)
+		print "os.system(com) execute zalay"
+	zf = zipfile.ZipFile("myzipfile111.zip", "w")	
+	for dirName, subdirList, fileList in os.walk(request.user):
+		print "dirName= " + dirName 
+		zf.write(dirName)
+		for fname in fileList:
+			zf.write(os.path.join(dirName, fname))
+			print "Zip is being written"
+        #print fname
+
+
+	os.rmdir(folder_name)
+	return render(request, 'sponsors.html')
