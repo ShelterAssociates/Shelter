8acb47622894ff4d8cee1866ca29d2f26dc1eb90
Amar Kamthe <amar.kamthe@soft-corner.com>
2017-11-21 16:53:05 +0530

    Command line script to accept user inputs and compress the images in the folder

--- scripts/compress_image.py
+++ scripts/compress_image.py
@@ -1,34 +1,54 @@
-#Compress all jpg file found in kobo
-import os
+"""
+Compress script :  Compress all the image file that get collected in some folder
+
+Input parameters 
+	-h : help
+	-f : folder path
+	-s : (optional) files above this size will get compressed (in MBs)
+	-q : (optional) Quality of image that needs to be maintained (in percentage)
+	-c : (optional) If specified it will compress the image 
+	     else just display the count. 
+	    
+"""
+import os, ast
 import sys
 from PIL import Image
+import argparse
 
-def compressMe(file, verbose=False):
-	filepath = os.path.join(os.getcwd(), file)
-	oldsize = os.stat(filepath).st_size
+def compressMe(filepath, quality):
+	original_size = os.stat(filepath).st_size / 1024
 	picture = Image.open(filepath)
-	dim = picture.size
-	
-	#set quality= to the preferred quality. 
-	#I found that 85 has no difference in my 6-10mb files and that 65 is the lowest reasonable number
-	picture.save("Compressed_"+file,"JPEG",optimize=True,quality=85) 
-	
-	newsize = os.stat(os.path.join(os.getcwd(),"Compressed_"+file)).st_size
-	percent = (oldsize-newsize)/float(oldsize)*100
-	if (verbose):
-		print "File compressed from {0} to {1} or {2}%".format(oldsize,newsize,percent)
-	return percent
+	picture.save(filepath,  optimize=True, quality=quality)
+	new_size = os.stat(filepath).st_size/1024
+	print "original size -" + str(original_size) +" : new size -" + str(new_size) + " filepath::" + filepath
 
-def getFiles(xml_root_path):
+
+def getFiles(xml_root_path, above_size, quality, compress_flag):
 	count = 0
 	for dirpath, dirs, files in os.walk(xml_root_path):
 		for filename in files:
-			if os.stat(os.path.join(dirpath,filename)).st_size/1024 > 2048:
+			filepath = os.path.join(dirpath, filename)
+			original_size = os.stat(filepath).st_size / 1024
+			if original_size > above_size:
+				if compress_flag:
+					compressMe(filepath, quality)
 				count += 1
-	print "Total number of files greater that 800 KB" + str(count)
+	print "Total number of files greater that "+str(above_size)+"MB - " + str(count)
 
 if __name__ == "__main__":
-	getFiles('')
+	parser = argparse.ArgumentParser(prog='compress_image', conflict_handler='resolve',
+									description = "Script to compress all the image files in the specified folder")
+	parser.add_argument('-f', '--folder', help='Folder path',required=True)
+	parser.add_argument('-s', '--size', type=int, default=500, help='compress files above size(in MBs)')
+	parser.add_argument('-q', '--quality', type=int, default=65, help='Quality to which the image needs to be compressed')
+	parser.add_argument('-c', '--compress', action='store_true', help='Compress the files. If not supplied it will just print the count')
+	args = parser.parse_args()
+	if not args.help:
+		folder_path = args.folder
+		above_size = args.size
+		quality = args.quality
+		compress_flag = args.compress
+		getFiles(folder_path, above_size, quality, compress_flag)
 
 
 
