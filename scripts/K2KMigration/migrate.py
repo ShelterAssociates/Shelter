d9e1988789103b4a501cda58e44d3418181d6669
Amar Kamthe <amarkamthe123@gmail.com>
2017-12-28 19:42:13 +0530

    Added code to replace old to new

--- scripts/K2KMigration/migrate.py
+++ scripts/K2KMigration/migrate.py
@@ -1,9 +1,12 @@
 ''''
-    Script to update PCMC RHS records for current place of defecation. 
+    Script to transfer data from one form to other.
+     format.json - JSON format that is accepted by the new form.
+     direct_mapping.json - JSON format - Fields that are directly mapped from new form to old form
+     
     To run include the folder in sys path
 
     import sys
-    sys.path.insert(0, 'scripts/update_kobo_records/')
+    sys.path.insert(0, 'scripts/K2KMigration/')
 '''
 
 import uuid
@@ -18,11 +21,13 @@ import xml.etree.ElementTree as ET
 kobo_survey = '87'
 root_folder_path = os.path.dirname(os.path.abspath(__file__))
 root_output_folder = os.path.join(root_folder_path, 'xml_output')
-output_folder_path = os.path.join(root_output_folder, 'PCMC', 'RHS')
+output_folder_path = os.path.join(root_output_folder, 'KMC', 'RHS')
 
+format = open(os.path.join(root_folder_path, 'format.json'),'r')
+direct_mapping = open(os.path.join(root_folder_path, 'direct_mapping.json'), 'r')
 
 def rhs_form():
-    pass
+    return json.loads(format.read())
 
 def queryToFetchRecords(kobo_survey, slum_code):
     print "Start fetching the details"
@@ -44,10 +49,10 @@ def queryToFetchRecords(kobo_survey, slum_code):
         record_sorted = sorted(val, key=lambda x: x['end'], reverse=True)
         record = record_sorted[0]
         create_data = rhs_xml_create(record)
+        print create_data
         status, replace_data = kmc_rhs_xml_replace(create_data)
 
         if status:
-            replace_data['__version__'] = record['__version__']
             xml_string = create_xml_string(replace_data, [], record['_xform_id_string'], record['_xform_id_string'],
                                            record['__version__'])
             file_name = 'RHS_Survey_Slum_Id_' + str(slum_code.replace('/', '')) + '_House_code_' + str(key)
@@ -57,18 +62,21 @@ def queryToFetchRecords(kobo_survey, slum_code):
 
 def rhs_xml_create(xml_record):
     gl_rhs_xml_dict = rhs_form()
+    mapping =  json.loads(direct_mapping.read())
     for key, val in xml_record.iteritems():
         key = str(key)
         if not key.startswith( '_' ):
-            split_key = key.split('/')
-            if len(split_key) == 1:
-                gl_rhs_xml_dict[split_key[0]] = val
-            if len(split_key) == 2:
-                gl_rhs_xml_dict[split_key[0]][split_key[1]] = val
-            if len(split_key) == 3:
-                gl_rhs_xml_dict[split_key[0]][split_key[1]][split_key[2]] = val
-            if len(split_key) == 4:
-                gl_rhs_xml_dict[split_key[0]][split_key[1]][split_key[2]][split_key[3]] = val
+            split_ky = key.split('/')
+            if split_ky[len(split_ky)-1] in mapping.keys():
+                split_key = mapping[split_ky[len(split_ky)-1]].split('/')
+                if len(split_key) == 1:
+                    gl_rhs_xml_dict[split_key[0]] = val
+                if len(split_key) == 2:
+                    gl_rhs_xml_dict[split_key[0]][split_key[1]] = val
+                if len(split_key) == 3:
+                    gl_rhs_xml_dict[split_key[0]][split_key[1]][split_key[2]] = val
+                if len(split_key) == 4:
+                    gl_rhs_xml_dict[split_key[0]][split_key[1]][split_key[2]][split_key[3]] = val
 
     return gl_rhs_xml_dict
 
@@ -76,7 +84,7 @@ def kmc_rhs_xml_replace(rhs_record):
     flag=False
     '''Replacement for the records
     '''
-    rhs_record['meta']['deprecatedID'] = rhs_record['meta']['instanceID']
+    #rhs_record['meta']['deprecatedID'] = rhs_record['meta']['instanceID']
     rhs_record['meta']['instanceID'] = 'uuid:' + str(uuid.uuid4())
     flag=True
     return flag, rhs_record
