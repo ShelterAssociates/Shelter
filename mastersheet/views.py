9c4b21c9972ccad7066151310005ac51a64a09b3
abhijitghate <abhijtsghate0605@gmail.com>
2018-01-05 18:34:59 +0530

    corrected

--- mastersheet/views.py
+++ mastersheet/views.py
@@ -17,10 +17,10 @@ import urllib2
 # Create your views here.
 def masterSheet(request):
     print "In masterSheet view...."
-    urlv = 'http://kc.shelter-associates.org/api/v1/data/54?query={"group_ce0hf58/slum_name":"272537890801"}'
+    urlv = ''
     print ("Sending Request to", urlv)
     kobotoolbox_request = urllib2.Request(urlv)
-    kobotoolbox_request.add_header('Authorization', "OAuth2 c213f2e7a3221171e8dd501f0fd8153ad95ecd93")
+    kobotoolbox_request.add_header('Authorization', "OAuth2 ")
     res = urllib2.urlopen(kobotoolbox_request)
     html = res.read()
     formdict = json.loads(html)
