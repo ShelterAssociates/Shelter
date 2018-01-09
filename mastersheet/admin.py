b3568db202b6c39a95d80556f2af8513622fae40
Amar Kamthe <amarkamthe123@gmail.com>
2018-01-09 18:18:16 +0530

    Registered models of mastersheet

--- mastersheet/admin.py
+++ mastersheet/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(VendorType)
+admin.site.register(Vendor)
+admin.site.register(VendorHouseholdInvoiceDetail)
+admin.site.register(SBMUpload)
+admin.site.register(ToiletConstruction)
+admin.site.register(ActivityType)
+admin.site.register(CommunityMobilization)
