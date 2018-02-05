cfd277d21aef9efb4748534e1514438f9615a361
Amar Kamthe <amarkamthe123@gmail.com>
2018-02-05 18:34:42 +0530

    key changes as per kmc rhs data

--- scripts/K2KMigration/migrate.py
+++ scripts/K2KMigration/migrate.py
@@ -22,7 +22,7 @@ from bs4 import BeautifulSoup
 import re
 import datetime
 
-kobo_survey = '73'
+kobo_survey = "73"
 root_folder_path = os.path.dirname(os.path.abspath(__file__))
 root_output_folder = os.path.join(root_folder_path, 'xml_output')
 output_folder_path = os.path.join(root_output_folder, 'KMC', 'RHS')
@@ -72,7 +72,7 @@ def queryToFetchRecords(kobo_survey):
         record_sorted = sorted(val, key=lambda x: x['end'], reverse=True)
         flag=True
         for record_sort in record_sorted:
-            if record_sort['group_ce0hf58/Type_of_structure_occupancy'] == '01' or (record_sort['group_ce0hf58/Type_of_structure_occupancy']!= "01" and flag):
+            if record_sort['group_ce0hf58/Type_of_structure_occupancy'] == "01" or (record_sort['group_ce0hf58/Type_of_structure_occupancy']!= "01" and flag):
                 flag = False
                 record = record_sort
                 #record = record_sorted[0]
@@ -110,8 +110,8 @@ def rhs_xml_create(xml_record):
 
     return gl_rhs_xml_dict
 
-UNOCCUPIED_HOUSE = {'03' : '021', '04':'022', '05':'023', '06':'024', '07':'025'}
-FACILITY_OF_SOLID_WASTE = {'01':'04', '02':'01', '03':'02', '04':'03', '05':'04', '06':'05', '07':'06'}
+UNOCCUPIED_HOUSE = {"03" : "021", "04":"022", "05":"023", "06":"024", "07":"025"}
+FACILITY_OF_SOLID_WASTE = {"01":"04", "02":"01", "03":"02", "04":"03", "05":"04", "06":"05", "07":"06"}
 def kmc_rhs_xml_replace(rhs_record, record):
     flag=False
     '''Replacement for the records
@@ -121,13 +121,13 @@ def kmc_rhs_xml_replace(rhs_record, record):
     rhs_record['Enter_household_number_again'] = record['group_ce0hf58/house_no']
     rhs_record['meta']['instanceID'] = 'uuid:' + str(uuid.uuid4())
     # RHS section
-    if record['group_ce0hf58/Type_of_structure_occupancy'] in ['03','04','05','06','07']:
-        rhs_record['Type_of_structure_occupancy'] = '02'
+    if record['group_ce0hf58/Type_of_structure_occupancy'] in ["03","04","05","06","07"]:
+        rhs_record['Type_of_structure_occupancy'] = "02"
         rhs_record['Type_of_unoccupied_house'] = UNOCCUPIED_HOUSE[record['group_ce0hf58/Type_of_structure_occupancy']]
     else:
-        rhs_record['Type_of_structure_occupancy'] = '03' if record['group_ce0hf58/Type_of_structure_occupancy']=='02' else '01'
+        rhs_record['Type_of_structure_occupancy'] = "03" if record['group_ce0hf58/Type_of_structure_occupancy']=="02" else "01"
 
-    if rhs_record['Type_of_unoccupied_house'] and rhs_record['Type_of_unoccupied_house'] == '021':
+    if rhs_record['Type_of_unoccupied_house'] and rhs_record['Type_of_unoccupied_house'] == "021":
 
         parent_house = (record['group_ye18c77/group_ud4em45/what_is_the_full_name_of_the_family_head_'].strip())#.split(' ')
         #print record['group_ye18c77/group_ud4em45/what_is_the_full_name_of_the_family_head_']
@@ -135,24 +135,22 @@ def kmc_rhs_xml_replace(rhs_record, record):
             rhs_record['Parent_household_number'] = int(re.search('[0-9]+',parent_house).group(0))
 
     if rhs_record['Type_of_structure_occupancy'] != "01":
-        rhs_record['group_el9cl08']['Type_of_water_connection'] =''
-        rhs_record['group_og5bx85']['Full_name_of_the_head_of_the_household'] = ''
+        rhs_record['group_el9cl08']['Type_of_water_connection'] =""
+        rhs_record['group_og5bx85']['Full_name_of_the_head_of_the_household'] = ""
 
     if rhs_record['group_el9cl08']['Enter_the_10_digit_mobile_number'] and not re.search('^[0-9]{10}$|^[0-9]{12}$',
                                                                                          rhs_record[
                                                                                              'group_el9cl08'][
                                                                                              'Enter_the_10_digit_mobile_number']):
-        if rhs_record['group_el9cl08']['Enter_the_10_digit_mobile_number'] != '0':
-            print "Mobile no :" + rhs_record['group_el9cl08']['Enter_the_10_digit_mobile_number']
-        rhs_record['group_el9cl08']['Enter_the_10_digit_mobile_number'] = ''
+        rhs_record['group_el9cl08']['Enter_the_10_digit_mobile_number'] = ""
 
     if rhs_record['group_el9cl08']['Aadhar_number'] and not re.search('^[0-9]{12}$',
                                                                       rhs_record['group_el9cl08']['Aadhar_number']):
         #print "Aadhar"+ rhs_record['group_el9cl08']['Aadhar_number']
-        rhs_record['group_el9cl08']['Aadhar_number'] = ''
+        rhs_record['group_el9cl08']['Aadhar_number'] = ""
 
     if rhs_record['Type_of_structure_occupancy'] == "01":
-        rhs_record['group_og5bx85']['Type_of_survey'] = '01'
+        rhs_record['group_og5bx85']['Type_of_survey'] = "01"
 
         if rhs_record['group_el9cl08']['Number_of_household_members'] and int(
                 rhs_record['group_el9cl08']['Number_of_household_members']) > 20:
@@ -161,15 +159,15 @@ def kmc_rhs_xml_replace(rhs_record, record):
 
         if 'group_ye18c77/group_yw8pj39/house_area_in_sq_ft' in record:
             if record['group_ye18c77/group_yw8pj39/house_area_in_sq_ft'] < 100:
-                rhs_record['group_el9cl08']['House_area_in_sq_ft'] = '01'
+                rhs_record['group_el9cl08']['House_area_in_sq_ft'] = "01"
             elif 100 <= record['group_ye18c77/group_yw8pj39/house_area_in_sq_ft'] < 200 :
-                rhs_record['group_el9cl08']['House_area_in_sq_ft'] = '02'
+                rhs_record['group_el9cl08']['House_area_in_sq_ft'] = "02"
             elif 200 <= record['group_ye18c77/group_yw8pj39/house_area_in_sq_ft'] < 300:
-                rhs_record['group_el9cl08']['House_area_in_sq_ft'] = '03'
+                rhs_record['group_el9cl08']['House_area_in_sq_ft'] = "03"
             elif 300 <= record['group_ye18c77/group_yw8pj39/house_area_in_sq_ft'] < 400:
-                rhs_record['group_el9cl08']['House_area_in_sq_ft'] = '04'
+                rhs_record['group_el9cl08']['House_area_in_sq_ft'] = "04"
             elif record['group_ye18c77/group_yw8pj39/house_area_in_sq_ft'] >= 400:
-                rhs_record['group_el9cl08']['House_area_in_sq_ft'] = '05'
+                rhs_record['group_el9cl08']['House_area_in_sq_ft'] = "05"
         waste_facility =[]
 
         if 'group_ye18c77/group_yw8pj39/facility_of_waste_collection' in record and len(record['group_ye18c77/group_yw8pj39/facility_of_waste_collection'].split(' ')) > 0:
@@ -180,42 +178,44 @@ def kmc_rhs_xml_replace(rhs_record, record):
 
         # RHS and follow up survey data
         if 'group_ye18c77/group_yw8pj39/Current_place_of_defecation_toilet' in record:
-            if record['group_ye18c77/group_yw8pj39/Current_place_of_defecation_toilet'] == "01":
-                if 'group_ye18c77/group_yw8pj39/Have_you_applied_for_indiviual' in record and record[
-                    'group_ye18c77/group_yw8pj39/Have_you_applied_for_indiviual'] == '01':
-                    if 'group_ye18c77/group_yw8pj39/what_is_the_status_of_toilet_under_sbm_' in record and record[
-                        'group_ye18c77/group_yw8pj39/what_is_the_status_of_toilet_under_sbm_'] == '01':
-                        rhs_record['group_oi8ts04']['C1'] = "01"
-                elif 'group_ye18c77/group_yw8pj39/Have_you_applied_for_indiviual' in record and record[
-                    'group_ye18c77/group_yw8pj39/Have_you_applied_for_indiviual'] == '02':
-                    rhs_record['group_oi8ts04']['C2'] = "01"
-            elif record['group_ye18c77/group_yw8pj39/Current_place_of_defecation_toilet'] != "01":
-                DEFECATION = {'03': '09', '04': '10', '05': '11', '07': '12'}
-                if record['group_ye18c77/group_yw8pj39/Current_place_of_defecation_toilet'] in DEFECATION.keys():
-                    rhs_record['group_oi8ts04']['C3'] = DEFECATION[
-                        record['group_ye18c77/group_yw8pj39/Current_place_of_defecation_toilet']]
+            # if record['group_ye18c77/group_yw8pj39/Current_place_of_defecation_toilet'] == "01":
+            #     if 'group_ye18c77/group_yw8pj39/Have_you_applied_for_indiviual' in record and record[
+            #         'group_ye18c77/group_yw8pj39/Have_you_applied_for_indiviual'] == "01":
+            #         if 'group_ye18c77/group_yw8pj39/what_is_the_status_of_toilet_under_sbm_' in record and record[
+            #             'group_ye18c77/group_yw8pj39/what_is_the_status_of_toilet_under_sbm_'] == "01":
+            #             rhs_record['group_oi8ts04']['C1'] = "01"
+            #     elif 'group_ye18c77/group_yw8pj39/Have_you_applied_for_indiviual' in record and record[
+            #         'group_ye18c77/group_yw8pj39/Have_you_applied_for_indiviual'] == "02":
+            #         rhs_record['group_oi8ts04']['C2'] = "01"
+            # elif record['group_ye18c77/group_yw8pj39/Current_place_of_defecation_toilet'] != "01":
+            DEFECATION = {"01":"01","03": "09", "04": "10", "05": "11", "07": "12"}
+            if record['group_ye18c77/group_yw8pj39/Current_place_of_defecation_toilet'] in DEFECATION.keys():
+                rhs_record['group_oi8ts04']['C3'] = DEFECATION[
+                    record['group_ye18c77/group_yw8pj39/Current_place_of_defecation_toilet']]
+            else:
+                print "ERRORRRRR :  "+record['group_ye18c77/group_yw8pj39/Current_place_of_defecation_toilet']
 
         if 'group_ye18c77/group_yw8pj39/If_Own_household_Toilet_type_' in record and record[
-            'group_ye18c77/group_yw8pj39/If_Own_household_Toilet_type_'] == '01':
+            'group_ye18c77/group_yw8pj39/If_Own_household_Toilet_type_'] == "01":
             rhs_record['group_oi8ts04']['Type_of_SBM_toilets'] = "01"
 
-        TOILET_UNDER_SBM = {'01': '01', '02': '03', '03': '04', '04': '05'}
+        TOILET_UNDER_SBM = {"01": "01", "02": "03", "03": "04", "04": "05"}
         if 'group_ye18c77/group_yw8pj39/what_is_the_status_of_toilet_under_sbm_' in record:
             rhs_record['group_oi8ts04']['Status_of_toilet_under_SBM'] = TOILET_UNDER_SBM[
                 record['group_ye18c77/group_yw8pj39/what_is_the_status_of_toilet_under_sbm_']]
 
-        TOILET_PREFERENCE = {'01': '01', '02': '02', '03': '02', '04': '02'}
+        TOILET_PREFERENCE = {"01": "01", "02": "02", "03": "02", "04": "02"}
         if 'group_ye18c77/group_yw8pj39/type_of_toilet_preference' in record:
             rhs_record['group_oi8ts04']['What_kind_of_toilet_would_you_like'] = TOILET_PREFERENCE[record[
                 'group_ye18c77/group_yw8pj39/type_of_toilet_preference']]
 
-        TOILET_CONNECTED_TO = {'01': '01', '02': '02', '03': '03', '04': '04', '05': '05', '06': '06', '07': '08',
-                               '08': '08', '09': '07'}
+        TOILET_CONNECTED_TO = {"01": "01", "02": "02", "03": "03", "04": "04", "05": "05", "06": "06", "07": "08",
+                               "08": "08", "09": "07"}
         if 'group_ye18c77/group_yw8pj39/where_the_individual_toilet_is_connected_to_' in record:
             rhs_record['group_oi8ts04']['What_is_the_toilet_connected_to'] = TOILET_CONNECTED_TO[
                 record['group_ye18c77/group_yw8pj39/where_the_individual_toilet_is_connected_to_']]
 
-        OPEN_DEFECATION = {'01': '01', '02': '02', '03': '04', '04': '05'}
+        OPEN_DEFECATION = {"01": "01", "02": "02", "03": "04", "04": "05"}
         if 'group_ye18c77/group_yw8pj39/does_any_member_of_your_family_go_for_open_defecation_' in record:
             rhs_record['group_oi8ts04']['OD1'] = OPEN_DEFECATION[
                 record['group_ye18c77/group_yw8pj39/does_any_member_of_your_family_go_for_open_defecation_']]
@@ -266,19 +266,19 @@ def create_xml_file(xml_root, filename, folderpath):
 
     xml_tree.write(xml_file, xml_declaration=True, encoding='utf-8', method="xml")
 
-    fp_read = open(xml_file, 'r')
-    soup = BeautifulSoup((fp_read.read().replace('\n','')), 'xml')
-    fp_read.close()
-    try:
-        d = soup.prettify()
-        #print soup.prettify()
-        fp_write = open(xml_file, 'w')
-        fp_write.write(d)
-        fp_write.close()
-    except Exception as e:
-        print "Error"
-        print e
-        print xml_file
+    # fp_read = open(xml_file, 'r')
+    # soup = BeautifulSoup((fp_read.read().replace('\n','')), 'xml')
+    # fp_read.close()
+    # try:
+    #     d = soup.prettify()
+    #     #print soup.prettify()
+    #     fp_write = open(xml_file, 'w')
+    #     fp_write.write(d)
+    #     fp_write.close()
+    # except Exception as e:
+    #     print "Error"
+    #     print e
+    #     print xml_file
 
 
     log_msg = "created xml file : " + xml_file
