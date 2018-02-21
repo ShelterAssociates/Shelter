ea4d0378cb99275cf4af49f04c07b63ebfe169dc
Amar Kamthe <amarkamthe123@gmail.com>
2018-02-21 12:52:39 +0530

    code changes for calculating top level text field

--- static/js/collapse_household_code.js
+++ static/js/collapse_household_code.js
@@ -13,7 +13,7 @@ function less_click(element){
     $(element).parent().parent().parent().find('div.morelist').show();
 }
 $(document).ready(function(){
-
+    //Used for the list view for household codes
     function update_view(){
         if($('#result_list').length > 0){
             $.each($('.field-household_code'), function(index, house){
@@ -23,22 +23,61 @@ $(document).ready(function(){
             });
         }
     }
-
     update_view();
 
-    //$("textArea[name=household_code], div.field-household_code p.help").addClass('hide');
-    //var household = $("textArea[name=household_code]").val();
-    //$("textArea[name=household_code]").parent().append('<p class="householdcode">'+household+'</p>');
-    //$("input[name=_save]").click(function(){
-    //    var data = "";
-    //    $.each($("td.field-household_code"), function(index, element){
-    //           var d = $(element).find('textArea').val();
-    //           if (d != "")
-    //           {
-    //                data += d.slice(d.indexOf('[')+1, d.indexOf(']')) + ',';
-    //           }
-    //   });
-    //    $("textArea[name=household_code]").val("["+data.slice(0,data.length-1)+"]");
-    //});
+    //Used for add/edit for sponsor project details page
+    if ($("#id_sponsor option:selected").text() != "SBM Toilets" && $("#id_sponsor").val() != ""){
+        $("textArea[name=household_code], div.field-household_code p.help").addClass('hide');
+        var household = $("textArea[name=household_code]").val();
+        $("textArea[name=household_code]").parent().append('<p class="householdcode">'+household+'</p>');
+        $("input[name=_save]").click(function(){
+            var data = "";
+            var flag = false;
+            $.each($("td.field-household_code"), function(index, element){
+                   var element_text = $(element).find('textArea')
+                   var d = element_text.val();
+                   var duplicate = check_for_duplicate(element_text);
+                   if (duplicate.length > 0){
+                    flag=true;
+                   }
+                   if (d != "")
+                   {
+                        data += d.slice(d.indexOf('[')+1, d.indexOf(']')) + ',';
+                   }
+           });
+            $("textArea[name=household_code]").val("["+data.slice(0,data.length-1)+"]");
+            if (flag){
+             return false;
+            }
+        });
+    }
+//Checking for duplicate values within the list.
+//    function check_for_duplicate(element){
+//       var d = $(element).val();
+//       var data = null;
+//       var duplicate = []
+//       if (d != "")
+//       {
+//         data = d.slice(d.indexOf('[')+1, d.indexOf(']'));
+//         data = data.split(',')
+//         house = household.slice(household.indexOf('[')+1, household.indexOf(']'));
+//         house = house.replace(' ','').split(',')
+//         $.each(data, function(index, val){
+//            val = val.replace('\n','').replace(' ', '');
+//            if (val in house){
+//                duplicate.append(val);
+//            }
+//         });
+//       }
+//       return duplicate;
+//    }
+//    $("td.field-household_code").on('blur','textArea',function(element){
+//       var duplicate = check_for_duplicate($($(element)[0].target));
+//       if (duplicate.length > 0)
+//       {
+//        alert("The households "+duplicate.join(',') + " are duplicated.");
+//       }
+//
+//    });
 
 });
