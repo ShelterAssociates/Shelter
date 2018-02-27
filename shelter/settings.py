03bc47458936b7f73041c32e2b16f79a3f619a3a
abhijitghate <abhijtsghate0605@gmail.com>
2018-02-27 15:11:17 +0530

    base url dir changed

--- shelter/settings.py
+++ shelter/settings.py
@@ -107,13 +107,13 @@ from local_settings import *
 STATIC_URL = '/static/'
 
 
-STATIC_ROOT = os.path.join( BASE_DIR, 'static')
+STATIC_ROOT = ''
 
 STATICFILES_DIRS = (
    # Put strings here, like "/home/html/static" or "C:/www/django/static".
    # Always use forward slashes, even on Windows.
    # Don't forget to use absolute paths, not relative paths.
-   #os.path.join( BASE_DIR, 'static'),
+   os.path.join( BASE_DIR, 'static'),
 )
 
 MEDIA_URL = '/media/'
