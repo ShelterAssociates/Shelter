11881873ceacc0ec5000f8afa650ace6144f91de
abhijitghate <abhijtsghate0605@gmail.com>
2018-02-27 15:07:17 +0530

    new views for buttons and columns

--- mastersheet/urls.py
+++ mastersheet/urls.py
@@ -6,6 +6,8 @@ from django.contrib.auth import views as auth_views
 from mastersheet.views import *
 
 urlpatterns = [
+    url(r'^columns/$', define_columns, name="define_columns"),
+    url(r'^buttons/$', define_buttons, name="define_buttons"),
     url(r'^show/mastersheet/$', renderMastersheet, name="renderMastersheet"),
     url(r'^list/show/$', masterSheet, name="masterSheet"),
     #url(r'^$', masterSheet, name='masterSheet'),
