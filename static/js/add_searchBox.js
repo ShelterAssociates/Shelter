372246d4d3d193f63388db0d97fb11785b739d63
abhijitghate <abhijtsghate0605@gmail.com>
2018-02-27 15:13:37 +0530

    search box functionality seperated in a javascript

---
+++ static/js/add_searchBox.js
@@ -0,0 +1,21 @@
+function add_search_box(){
+                console.log("in search box...");
+                $('#example tfoot tr').empty();
+                var numCols = $('#example thead th').length;
+                var append_this = '<tfoot><tr>';
+                for(i = 0; i < numCols; i++){
+                    append_this = append_this + '<th></th>';
+                }
+                append_this = append_this + '</tr></tfoot>';
+                $('#example').each(function(){
+                    $(this).append(append_this);
+                });
+
+                $('#example tfoot th').each( function (index,element) {
+                    var title = $(this).text()  ;
+                    something = $(this).parent().parent().parent();
+                    title=(something.find('thead tr th:eq('+index+')')[0].innerText);
+                    $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+                } );
+
+            }
