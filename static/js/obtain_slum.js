bb8815583a105f22b601ce8fea60338b7c80314b
abhijitghate <abhijtsghate0605@gmail.com>
2018-03-19 19:37:18 +0530

    sending selected slum to backend

--- static/js/obtain_slum.js
+++ static/js/obtain_slum.js
@@ -4,12 +4,14 @@ $(document).ready(function(){
 
                 $("#btnFetch").click(function(){
                     var form = $("#slum_form").serialize();
+
                     var csrf_token = document.getElementsByName("csrfmiddlewaretoken")[0].value;
                     $.ajax({
                             type:"POST",
                             url:"http://127.0.0.1:8000/mastersheet/list/show/",
                             data:{'form':form , 'csrfmiddlewaretoken':csrf_token},
                             dataType: "json",
+
                     });
                 });
             });
