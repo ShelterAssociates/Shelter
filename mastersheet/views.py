0fa3a615bef58827f619936080f2fb2fd8d8f321
Amar Kamthe <amarkamthe123@gmail.com>
2018-04-03 15:39:58 +0530

    Added view to return status of sync data from kobotoolbox

--- mastersheet/views.py
+++ mastersheet/views.py
@@ -12,7 +12,7 @@ from django.conf import settings
 from django.db.models.signals import pre_save,post_save
 from django.dispatch import receiver
 from django.http import JsonResponse
-
+from mastersheet.daily_reporting_sync import ToiletConstructionSync, CommunityMobilizaitonSync
 
 
 
@@ -544,8 +544,33 @@ def update_status(sender ,instance, **kwargs):
 
 
 
+def sync_kobo_data(request,slum_id):
+    data={}
+    try:
+        slum = Slum.objects.get(id=slum_id)
+        user = request.user
+        toilet_const = ToiletConstructionSync(slum, user)
+        t_data = toilet_const.fetch_data()
+        com_mobilization = CommunityMobilizaitonSync(slum, user)
+        c_data = com_mobilization.fetch_data()
 
+        if any(t_data) and any(c_data):
+            if t_data[2] > c_data[2]:
+                toilet_const.update_sync_info(t_data[2])
+            else:
+                com_mobilization.update_sync_info(c_data[2])
+        elif any(t_data) and not any(c_data):
+            toilet_const.update_sync_info(t_data[2])
+        elif not any(t_data) and any(c_data):
+            com_mobilization.update_sync_info(c_data[2])
 
+        data['flag'] = True
+        data['msg'] = "Data successfully synced for slum:"+slum.name
+        data['msg'] += "\n Total records updated" + str(t_data+c_data)
+    except Exception as e:
+        data['flag']=False
+        data['msg'] = "Error occurred while sync from kobo. Please contact administrator." +str(e)
+    return HttpResponse(json.dumps(data), content_type="application/json")
 
 
 
