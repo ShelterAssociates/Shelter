4157fb68cb21fc65806ce1ee98e9cb10f8029d0b
Amar Kamthe <amarkamthe123@gmail.com>
2018-04-06 18:27:34 +0530

    Removed unused URL's

--- mastersheet/urls.py
+++ mastersheet/urls.py
@@ -9,10 +9,7 @@ urlpatterns = [
     url(r'^delete_selected/$', delete_selected, name="delete_selected"),
     url(r'^files/$', file_ops, name="file_ops"),
     url(r'^columns/$', define_columns, name="define_columns"),
-    url(r'^buttons/$', define_buttons, name="define_buttons"),
     url(r'^show/mastersheet/$', renderMastersheet, name="renderMastersheet"),
     url(r'^list/show/$', masterSheet, name="masterSheet"),
-    #url(r'^$', masterSheet, name='masterSheet'),
     url(r'^sync/slum/(?P<slum_id>\d+)$', sync_kobo_data, name='sync_kobo_data')
-
 ]
