af6bca2ba65e31afac291c8a3098cf751cf247aa
abhijitghate <abhijitsghate0605@gmail.com>
2018-08-09 15:17:12 +0530

    link for Matersheet Report view is added

--- mastersheet/urls.py
+++ mastersheet/urls.py
@@ -6,6 +6,7 @@ from django.contrib.auth import views as auth_views
 from mastersheet.views import *
 
 urlpatterns = [
+	url(r'^report_table/$', give_report_table_numbers, name="give_report_table_numbers"),
 	url(r'^details/$', give_details, name="give_details"),
 	url(r'^report/$', render_report, name="render_report"),
 	url(r'^show/report/$', create_report, name="create_report"),
