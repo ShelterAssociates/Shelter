04b6b7f52faf3b475196b42bf27be145193c17f5
Akash <akash.bsy19@gmail.com>
2019-03-29 18:24:21 +0530

    removed 'SBM Toilets' from sponspor project columns to avoid duplication

--- DBScripts/vw_sponsordetails.sql
+++ DBScripts/vw_sponsordetails.sql
@@ -24,5 +24,7 @@ SELECT
 FROM sponsor_sponsorprojectdetails as sd, sponsor_sponsorproject as sd2, sponsor_sponsor as sd1, master_slum as s, master_city as mcc, master_cityreference as c, master_electoralward as me,
 master_administrativeward as ma
 
-WHERE sd1.id = sd.sponsor_id AND sd2.id = sd.sponsor_project_id AND sd.slum_id = s.id AND s.electoral_ward_id= me.id AND me.administrative_ward_id=ma.id AND mcc.id = ma.city_id AND c.id = mcc.name_id
+WHERE sd1.id = sd.sponsor_id AND sd2.id = sd.sponsor_project_id AND sd.slum_id = s.id AND s.electoral_ward_id= me.id 
+AND me.administrative_ward_id=ma.id AND mcc.id = ma.city_id AND c.id = mcc.name_id
+AND sd1.organization_name <> 'SBM Toilets'
 
