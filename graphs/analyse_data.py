b9cda0bcfb34812ba33ef10c862aefc9f9319dd6
shelter <ashubhu13691@gmail.com>
2019-08-27 18:23:11 +0530

    data collection and storage for waste section

--- graphs/analyse_data.py
+++ graphs/analyse_data.py
@@ -1,6 +1,9 @@
+
+from __future__ import division
 from django.shortcuts import get_object_or_404
 from graphs.models import *
 from master.models import Slum
+
 
 class RHSData(object):
     def __init__(self, slum):
@@ -15,7 +18,8 @@ class RHSData(object):
         return self.get_household_count() * 4
 
     def get_waste_facility(self, facility):
-        waste_facility = filter(lambda x: 'group_el9cl08/Facility_of_solid_waste_collection' in x.rhs_data and x.rhs_data['group_el9cl08/Facility_of_solid_waste_collection'] == facility,self.household_data)
+        waste_facility = filter(lambda x: 'group_el9cl08/Facility_of_solid_waste_collection' in x.rhs_data
+                        and x.rhs_data['group_el9cl08/Facility_of_solid_waste_collection'] == facility,self.household_data)
         return len(waste_facility)
 
     def get_water_coverage(self, type):
@@ -24,29 +28,15 @@ class RHSData(object):
                 'group_el9cl08/Type_of_water_connection'] == type, self.household_data)
         return len(water_coverage)
 
-    #General Information data
-    def get_household_member_size(self):
-        household_member = filter(lambda x:'group_el9cl08/Number_of_household_members' in x.rhs_data and int(x.rhs_data['group_el9cl08/Number_of_household_members'])<20, self.household_data)
-        return sum(map(lambda x:int(x.rhs_data['group_el9cl08/Number_of_household_members']), household_member)) / len(household_member)
-
-    def get_slum_area_size(self):
-        return int(self.slum_data.rim_data['group_zl6oo94/group_wb1hp47/approximate_area_of_the_settle'])/1000 if 'group_zl6oo94/group_wb1hp47/approximate_area_of_the_settle' in self.slum_data.rim_data else 0
-
-    def get_tenement_density(self):
-        area_size = self.get_slum_area_size()
-        return self.get_slum_population() / area_size if area_size!=0 else 0
-
-    def get_sex_ratio(self):
-        return 0
-
     #Waste data
-    def get_perc_of_waste_collection(self, facility='Door to door waste collection'):
+    def get_perc_of_waste_collection(self, facility ='Door to door waste collection'):
         """
         facility ['Door to door waste collection', 'Open space', 'Garbage bin', 'ULB service']
         :param facility:
         :return:
         """
-        return (self.get_waste_facility(facility) / self.get_household_count()) * 100
+        percent = (self.get_waste_facility(facility) / self.get_household_count()) * 100 if self.get_household_count() !=0 else 0
+        return (percent)
 
     #Water data
     def get_perc_of_water_coverage(self, type='Individual connection'):
@@ -56,3 +46,22 @@ class RHSData(object):
         :return:
         """
         return (self.get_water_coverage(type) / self.get_household_count()) * 100
+
+    # General Information data
+    def get_household_member_size(self):
+        household_member = filter(lambda x: 'group_el9cl08/Number_of_household_members' in x.rhs_data
+                                            and int(x.rhs_data['group_el9cl08/Number_of_household_members']) < 20,
+                                  self.household_data)
+        return sum(map(lambda x: int(x.rhs_data['group_el9cl08/Number_of_household_members']), household_member)) \
+               / len(household_member) if len(household_member) != 0 else 0
+
+    def get_slum_area_size(self):
+        return int(self.slum_data.rim_data['General']['approximate_area_of_the_settle']) / 1000 \
+            if 'approximate_area_of_the_settle' in self.slum_data.rim_data['General'] else 0
+
+    def get_tenement_density(self):
+        area_size = self.get_slum_area_size()
+        return self.get_slum_population() / area_size if area_size != 0 else 0
+
+    def get_sex_ratio(self):
+        return 0
--- graphs/dashboard_card.py
+++ graphs/dashboard_card.py
@@ -4,28 +4,51 @@ Script to get aggregated data.
 from graphs.models import *
 from analyse_data import *
 from master.models import *
-class DashboardData(RHSData):
+
+from django.http import HttpResponse
+import json
+
+class DashboardCard(RHSData):
     def __init__(self, slum):
-        super(DashboardData).__init__(self, slum)
+        super(DashboardCard,self).__init__(slum)
 
     def General_Info(self):
         member_size = self.get_household_member_size()
         tenement_density = self.get_tenement_density()
         sex_ratio = self.get_sex_ratio()
-        return (member_size, tenement_density, sex_ratio)
+        household_count = self.get_household_count()
+        return (member_size, tenement_density,household_count)
 
     def save_general(self):
         """Save information to database"""
-        (member_size, tenement_density, sex_ratio) = self.General_Info()
+        (member_size, tenement_density,household_count) = self.General_Info()
+        to_save = DashboardData.objects.update_or_create(slum = self.slum , defaults = {'tenement_density' : tenement_density,
+                                    'member_size' : member_size, 'household_count':household_count,
+                                    'city_id': self.slum.electoral_ward.administrative_ward.city.id})
+                                    # 'houehold_member_count':0, # remove houehold_member_count from model as well
 
+    def Waste_Info(self):
+        door_to_door_percent = self.get_perc_of_waste_collection('Door to door waste collection')
+        openspace_percent= self.get_perc_of_waste_collection('Open space')
+        gutter_facility = self.get_perc_of_waste_collection('inside gutter')
+        canal_facility = self.get_perc_of_waste_collection('along/inside canal')
+        No_facility_percent = (openspace_percent+gutter_facility+canal_facility)/3
+        return (door_to_door_percent, No_facility_percent,openspace_percent)
+
+    def save_waste(self):
+        (door_to_door_percent, No_facility_percent,openspace_percent) = self.Waste_Info()
+        to_save = DashboardData.objects.update_or_create(slum =self.slum, defaults={'no_facility_per': No_facility_percent,
+                                'door_to_door_percent': door_to_door_percent,
+                                'openspace_percent': openspace_percent })
 
 def dashboard_data_Save(city):
     slums = Slum.objects.filter(electoral_ward__administrative_ward__city__id__in = [city])
     for slum in slums:
         try:
-            dashboard_data = DashboardData(slum.id)
+            dashboard_data = DashboardCard(slum.id)
             dashboard_data.save_general()
+            dashboard_data.save_waste()
         except Exception as e:
-            print(e)
+            print 'Exception in dashboard_sata_save',(e)
 
 dashboard_data_Save(4)
