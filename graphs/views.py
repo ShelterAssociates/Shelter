a981fed6c5ba7bb5c63e9d1882d608b51e9538e8
amarkamthe <amarkamthe123@gmail.com>
2019-08-28 13:05:05 +0530

    Added code to get scores card data

--- graphs/views.py
+++ graphs/views.py
@@ -1,10 +1,53 @@
 from django.shortcuts import render
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from itertools import groupby
+from django.db.models import Avg
+from graphs.models import *
 from master.models import *
-
+import json
 
 @login_required(login_url='/accounts/login/')
 def graphs_display(request, graph_type):
     custom_url = settings.GRAPHS_BUILD_URL % settings.GRAPH_DETAILS[graph_type][1::-1]
     return render(request, 'graphs.html', {'custom_url':custom_url})
+
+def get_dashboard_card(request, key):
+    output_data = {'city':{}, 'admin_ward':{}, 'electoral_ward':{}, 'slum':{}}
+    select_clause = ['totalscore_percentile', 'general_percentile', 'drainage_percentile',
+                     'road_percentile', 'toilet_percentile', 'water_percentile', 'waste_percentile']
+
+    city = get_object_or_404(City, pk=key)
+
+    #City level
+    output_data['city'][city.name.city_name] = {'scores':{}, 'cards':{}}
+    qol_scores = QOLScoreData.objects.filter(city=city)
+    for clause in select_clause:
+        output_data['city'][city.name.city_name]['scores'][clause] = qol_scores.aggregate(Avg(clause))[clause + '__avg']
+
+    #Administrative ward calculations
+    for admin_ward in AdministrativeWard.objects.filter(city=city):
+        output_data['admin_ward'][admin_ward.name] = {'scores': {}, 'cards':{}}
+        qol_scores = QOLScoreData.objects.filter(slum__electoral_ward__administrative_ward=admin_ward)
+        for clause in select_clause:
+            output_data['admin_ward'][admin_ward.name]['scores'][clause] = qol_scores.aggregate(Avg(clause))[clause + '__avg']
+
+    #Electoral ward calculations
+    for electoral_ward in ElectoralWard.objects.filter(administrative_ward__city=city):
+        output_data['electoral_ward'][electoral_ward.name] = {'scores': {}, 'cards':{}}
+        qol_scores = QOLScoreData.objects.filter(slum__electoral_ward=electoral_ward)
+        for clause in select_clause:
+            output_data['electoral_ward'][electoral_ward.name]['scores'][clause] = qol_scores.aggregate(Avg(clause))[clause + '__avg']
+
+    #Slum level calculations
+    output_data['slum'] = {'scores':{}}
+    select_clause.append('slum__name')
+    qol_scores = QOLScoreData.objects.filter(city = city).values(*select_clause)
+    qol_scores = groupby(qol_scores, key=lambda x:x['slum__name'])
+    qol_scores = {key : {'scores': list(values)[0], 'cards':{}} for key, values in qol_scores}
+    output_data['slum'] = qol_scores
+
+    return HttpResponse(json.dumps(output_data),content_type='application/json')
+
