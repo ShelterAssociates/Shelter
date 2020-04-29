494e7d3ac2f69d73e21ebcd2772fd69647f0bfff
akku-09 <aakanksham2612@gmail.com>
2020-04-29 17:05:15 +0530

    Complete Component API

--- component/serializers.py
+++ component/serializers.py
@@ -6,15 +6,15 @@ from master.models import Slum
 
 class MetadataSerializer(serializers.ModelSerializer):
 
-    components = serializers.SerializerMethodField('component_count')
+    count_of_component = serializers.SerializerMethodField('component_count')
 
-    def component_count(self,slum_id):
+    def component_count(self,obj):
         request_object = self.context['request']
         shelter_slum_id = request_object.query_params.get('slum_id')
-        slum_data = Slum.objects.filter(id = shelter_slum_id)
-        print slum_data
-        comp = slum_data.components.count()
-        print comp
+        slum = Slum.objects.get(id=shelter_slum_id)
+        total_component_count = slum.components.filter(metadata_id=obj)
+        return total_component_count.count()
+
     class Meta:
         model = Metadata
-        fields = ['name','level','blob','type','components']
+        fields = ['id','name','level','blob','type','count_of_component']
