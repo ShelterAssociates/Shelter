8c717bb5c2c026212d45a5233b2ab9aa58c9fd25
akku-09 <aakanksham2612@gmail.com>
2020-05-12 20:35:06 +0530

    API to fetch co-ordinates 1st commit

--- component/api.py
+++ component/api.py
@@ -2,11 +2,14 @@ from string import count
 
 from rest_framework import viewsets
 
-from .serializers import MetadataSerializer
+from .serializers import MetadataSerializer,ComponentSerializer
 
-from .models import Metadata
+from .models import Metadata,Component
 class MetadataViewSet(viewsets.ModelViewSet):
     queryset = Metadata.objects.filter(type = 'C')
     serializer_class = MetadataSerializer
 
+class ComponentViewSet(viewsets.ModelViewSet):
+    queryset = Component.objects.order_by('id')
+    serializer_class = ComponentSerializer
 
--- component/serializers.py
+++ component/serializers.py
@@ -14,7 +14,25 @@ class MetadataSerializer(serializers.ModelSerializer):
         slum = Slum.objects.get(id=shelter_slum_id)
         total_component_count = slum.components.filter(metadata_id=obj)
         return total_component_count.count()
-
     class Meta:
         model = Metadata
         fields = ['id','name','level','blob','type','count_of_component']
+
+class ComponentSerializer(serializers.ModelSerializer):
+    component = serializers.SerializerMethodField('component_list')
+
+    def component_list(self, obj):
+        request_object = self.context['request']
+        shelter_slum_id = request_object.query_params.get('slum_id')
+        shelter_component_id = request_object.query_params.get('component_id')
+        print(shelter_slum_id)
+        print(shelter_component_id)
+        slum = Slum.objects.get(id=shelter_slum_id)
+        print(slum)
+        comp = slum.components.filter(id=shelter_component_id)
+        return comp
+
+    class Meta:
+        model = Component
+        fields = ['id','component']
+
