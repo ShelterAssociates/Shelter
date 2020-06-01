301ee730e753c36226f2bc1d7e71fd283d49570b
amarkamthe <amarkamthe123@gmail.com>
2020-06-01 19:35:58 +0530

    Added authentication to access city

--- master/api.py
+++ master/api.py
@@ -1,10 +1,22 @@
 from django.http import request
 from rest_framework import viewsets
-
+from rest_framework.permissions import IsAuthenticated
+from django.core.exceptions import PermissionDenied
 from .serializers import CitySerializer
 from .models import City
 
 class CityViewset(viewsets.ModelViewSet):
     serializer_class = CitySerializer
+    permission_classes = (IsAuthenticated,)
     queryset = City.objects.order_by('id')
+
+    def get_queryset(self):
+        user = self.request.user
+        filter_queryset= {}
+        if not user.is_superuser:
+            filter_queryset['name__city_name__in'] = map(lambda x: x.split(':')[-1], user.groups.values_list('name', flat=True))
+        queryset = City.objects.filter(**filter_queryset)
+        if queryset.count() <=0:
+            raise PermissionDenied("User do not have access to data")
+        return queryset
 
