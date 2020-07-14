d2f9ed10719166a5c896e0bfd329d217b323c033
amarkamthe <amarkamthe123@gmail.com>
2020-07-14 13:30:13 +0000

    Changes as per the new python environment

--- deploy/shelter.sh
+++ deploy/shelter.sh
@@ -7,7 +7,7 @@ NUM_WORKERS=4
 USER=ubuntu
 GROUP=ubuntu
 cd /srv/Shelter/
-source /srv/Shelter/ENV/bin/activate
+source /srv/Shelter/ENV3/bin/activate
 #pip freeze
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec /srv/Shelter/ENV/bin/gunicorn -b 0.0.0.0:8011 -w $NUM_WORKERS --timeout 1800 --user=$USER --group=$GROUP --log-level=info --log-file=$LOGFILE 2>>$LOGFILE shelter.wsgi:application
+exec /srv/Shelter/ENV3/bin/gunicorn -b 0.0.0.0:8011 -w $NUM_WORKERS --timeout 1800 --user=$USER --group=$GROUP --log-level=info --log-file=$LOGFILE 2>>$LOGFILE shelter.wsgi:application
--- deploy/update_cert.bash
+++ deploy/update_cert.bash
@@ -1,16 +1,17 @@
 #Script to renew the certificate file.
 #
-docker stop kobodocker_nginx_1
+#docker stop kobodocker_nginx_1
 
+sudo service nginx stop
 cd /opt/letsencrypt/
 
-sudo -H ./letsencrypt-auto certonly --standalone --renew-by-default -d dk.shelter-associates.org -d kc.shelter-associates.org -d enketo.shelter-associates.org -d app.shelter-associates.org -d graphs.shelter-associates.org
+#sudo -H ./letsencrypt-auto certonly --standalone --renew-by-default -d dk.shelter-associates.org -d kc.shelter-associates.org -d enketo.shelter-associates.org -d app.shelter-associates.org -d graphs.shelter-associates.org
+sudo -H ./letsencrypt-auto certonly --standalone --renew-by-default -d app.shelter-associates.org
 
-cd /srv/kobo-docker/secrets/
+#sudo cp /etc/letsencrypt/live/app.shelter-associates.org/fullchain.pem ssl.crt
 
-sudo cp /etc/letsencrypt/live/dk.shelter-associates.org/fullchain.pem ssl.crt
+#sudo cp /etc/letsencrypt/live/app.shelter-associates.org/privkey.pem ssl.key
 
-sudo cp /etc/letsencrypt/live/dk.shelter-associates.org/privkey.pem ssl.key
-
-docker start kobodocker_nginx_1
+sudo service nginx restart
+#docker start kobodocker_nginx_1
 
