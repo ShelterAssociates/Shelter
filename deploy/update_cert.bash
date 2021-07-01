18a92c2217eef6b330fb3aca35d1e92d8bb96bf7
amarkamthe <amarkamthe123@gmail.com>
2021-07-01 14:22:25 +0000

    added report to letsencrypt

--- deploy/update_cert.bash
+++ deploy/update_cert.bash
@@ -6,7 +6,7 @@ sudo service nginx stop
 cd /opt/letsencrypt/
 
 #sudo -H ./letsencrypt-auto certonly --standalone --renew-by-default -d dk.shelter-associates.org -d kc.shelter-associates.org -d enketo.shelter-associates.org -d app.shelter-associates.org -d graphs.shelter-associates.org
-sudo -H ./letsencrypt-auto certonly --standalone --renew-by-default -d app.shelter-associates.org
+sudo -H ./letsencrypt-auto certonly --standalone --renew-by-default -d app.shelter-associates.org -d report.shelter-associates.org
 
 #sudo cp /etc/letsencrypt/live/app.shelter-associates.org/fullchain.pem ssl.crt
 
