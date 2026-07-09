285f85cd1a4dae8625b4fdbca55962be4f6acc17
Vrundavan Sonje <sonjevrundavan2102@gmail.com>
2026-07-09 14:19:15 +0530

    KML parser updated for kml upload

--- master/KMLSlumParser.py
+++ master/KMLSlumParser.py
@@ -1,3 +1,4 @@
+import re
 from pykml import parser
 from django.contrib.gis.geos import GEOSGeometry
 from master.models import *
@@ -37,12 +38,28 @@ class KMLSlumParser(object):
         for coordinate in coordinates[:-1]:
             lst_coordinates.append(list(map(float, coordinate.split(","))))
         lst_coordinates = [lst_coordinates]
-        # Create geometry object as per type
         pnt = GEOSGeometry(
             '{ "type": "POLYGON" , "coordinates": ' + str(lst_coordinates) + "  }"
         )
 
         return pnt
+
+    @staticmethod
+    def _extended_data_dict(placemark):
+        """Build a {field_name_lower: value} lookup from SchemaData,
+        instead of relying on fixed index positions which break the
+        moment a file has a different field count/order."""
+        return {
+            sd.get("name").lower(): str(sd)
+            for sd in placemark.ExtendedData.SchemaData.SimpleData
+        }
+
+    @staticmethod
+    def _ward_number(text):
+        """Extract the numeric ward number embedded in a name like
+        'Admin Boundary 8' or 'Ward 8' or '8', for robust matching."""
+        match = re.search(r"\d+", str(text))
+        return match.group() if match else None
 
     def parse_admin(self, comp):
         self.data[comp] = {}
@@ -56,20 +73,27 @@ class KMLSlumParser(object):
         self.data[comp] = {}
         # Electoral ward data parsing
         for folder in self.folders.Placemark:
-            elect = {}
-            elec_ward_no = folder.name
-            admin_name = folder.ExtendedData.SchemaData.SimpleData[20]
-            name = folder.ExtendedData.SchemaData.SimpleData[21]
+            elec_ward_no = str(folder.name)
+            extendeddata = self._extended_data_dict(folder)
+
+            # 'Admin' field auto-detects the administrative ward this
+            # electoral boundary belongs to; 'Ward_No' is the electoral
+            # ward number/id
+            admin_name = extendeddata.get("admin", "")
+            ward_no = extendeddata.get("ward_no", elec_ward_no)
+            name = extendeddata.get("name") or admin_name or "Not Received"
+
             if name == "Not Received":
                 name += " (" + str(elec_ward_no) + ")"
+
             shape = self.get_coordinate(folder)
             elect = {
                 "name": name,
                 "admin_name": admin_name,
-                "ward_no": elec_ward_no,
+                "ward_no": ward_no,
                 "shape": shape,
             }
-            self.data[comp][elec_ward_no] = elect
+            self.data[comp][ward_no] = elect
 
     def parse_slum(self, comp):
         self.data[comp] = []
@@ -77,11 +101,9 @@ class KMLSlumParser(object):
         for folder in self.folders.Folder:
             ward = folder.name
             for slum in folder.Placemark:
-                slum_details = {}
-                name = slum.ExtendedData.SchemaData.SimpleData[16]
-                if name == "":
-                    name = slum.name
-                ward_no = slum.ExtendedData.SchemaData.SimpleData[18]
+                extendeddata = self._extended_data_dict(slum)
+                name = extendeddata.get("name") or str(slum.name)
+                ward_no = extendeddata.get("ward_no", "0")
                 shape = self.get_coordinate(slum)
                 slum_details = {
                     "name": name,
@@ -105,15 +127,19 @@ class KMLSlumParser(object):
             e = sorted(e.values(), key=lambda x: x["admin_name"])
 
             for admin_ward, electorals in groupby(e, key=lambda x: x["admin_name"]):
-
+                # Match electoral -> administrative ward by ward number
+                # instead of raw substring text, so formatting
+                # differences ("Admin Boundary 8" vs "Ward 8" vs "8")
+                # no longer cause a silent match failure
+                admin_num = self._ward_number(admin_ward)
                 admin_key = [
-                    x for x in self.data["admin"].keys() if x in str(admin_ward)
+                    x
+                    for x in self.data["admin"].keys()
+                    if self._ward_number(x) == admin_num and admin_num is not None
                 ]
                 if len(admin_key) > 0:
                     admin = self.data["admin"][admin_key[0]]
-                    print(admin_ward)
                     for electoral in electorals:
-                        print("\n\t\t" + str(electoral["name"]))
                         electoral_ward, flag = ElectoralWard.objects.update_or_create(
                             administrative_ward=admin,
                             name=electoral["name"],
@@ -126,8 +152,11 @@ class KMLSlumParser(object):
                         self.data["electoral"][
                             str(electoral["ward_no"])
                         ] = electoral_ward
-                # else:
-                # print admin_ward
+                else:
+                    print(
+                        "No matching administrative ward found for: "
+                        + str(admin_ward)
+                    )
 
         if "slum" in self.data.keys():
             for slum in self.data["slum"]:
@@ -136,7 +165,6 @@ class KMLSlumParser(object):
                     and str(slum["ward_no"]) in self.data["electoral"].keys()
                 ):
                     electoral = self.data["electoral"][str(slum["ward_no"])]
-                    # print electoral
                     slum_rec, flag = Slum.objects.update_or_create(
                         name=slum["name"],
                         electoral_ward=electoral,
